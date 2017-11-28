7220844c4648063c6d835eb5785e8b39578c669c
Dave Liu <liudave.liu@gmail.com>
2017-11-28 16:49:59 -0500

    Latest

--- experiment.py
+++ experiment.py
@@ -35,8 +35,8 @@ class Session(dj.Manual):
         -> Session
         trial   : smallint
         ---
-        start_time : decimal(6,4)  # (s)
-        end_time : decimal(6,4)  # (s)
+        start_time : decimal(8,4)  # (s)
+        end_time : decimal(8,4)  # (s)
         """
 		
 		
@@ -50,14 +50,18 @@ class Session(dj.Manual):
         TrialSettings=SessionData.flatten()[0][10]
         OriginalStateNamesByNumber=RawData.flatten()[0][0]
         OriginalStateData=RawData.flatten()[0][1]
+        OriginalEventData=RawData.flatten()[0][2]
         OriginalStateTimestamps=RawData.flatten()[0][3]
         OriginalEventTimestamps=RawData.flatten()[0][4]
 
-        for i in range(0, len(OriginalEventTimestamps)):
+        for i in range(0, len(OriginalStateTimestamps)):
             trial_instruction = 'left'
             early_lick = 'no early'
             outcome = 'ignore'
             GUI = TrialSettings[i][0]
+            SampleDur = GUI.flatten()[0][1]
+            DelayDur = GUI.flatten()[0][2]
+            AnswerPeriod = GUI.flatten()[0][3]
             ProtocolType = GUI.flatten()[0][10] # 1 Water-Valve-Calibration 2 Licking 3 Autoassist 4 No autoassist 5 DelayEnforce 6 SampleEnforce 7 Fixed
             Reversal = GUI.flatten()[0][13]
             StopLicking=np.where(OriginalStateNamesByNumber[i]=='StopLicking')[0]+1
@@ -66,7 +70,17 @@ class Session(dj.Manual):
             NoResponse=np.where(OriginalStateNamesByNumber[i]=='NoResponse')[0]+1
             EarlyLickDelay=np.where(OriginalStateNamesByNumber[i]=='EarlyLickDelay')[0]+1
             EarlyLickSample=np.where(OriginalStateNamesByNumber[i]=='EarlyLickSample')[0]+1
-            itemindex = np.where(OriginalStateData[i]==StopLicking)
+            PreSamplePeriod=np.where(OriginalStateNamesByNumber[i]=='PreSamplePeriod')[0]+1
+            SamplePeriod=np.where(OriginalStateNamesByNumber[i]=='SamplePeriod')[0]+1
+            DelayPeriod=np.where(OriginalStateNamesByNumber[i]=='DelayPeriod')[0]+1
+            ResponseCue=np.where(OriginalStateNamesByNumber[i]=='ResponseCue')[0]+1
+            startindex = np.where(OriginalStateData[i]==PreSamplePeriod)[0]
+            sampleindex = np.where(OriginalStateData[i]==SamplePeriod)[0]
+            delayindex = np.where(OriginalStateData[i]==DelayPeriod)[0]
+            responseindex = np.where(OriginalStateData[i]==ResponseCue)[0]
+            endindex = np.where(OriginalStateData[i]==StopLicking)[0]
+            lickleft = np.where(OriginalEventData[i]==69)[0]
+            lickright = np.where(OriginalEventData[i]==70)[0]
             if np.any(OriginalStateData[i]==Reward):
                 outcome = 'hit'
             elif np.any(OriginalStateData[i]==TimeOut):
@@ -80,21 +94,31 @@ class Session(dj.Manual):
                 if np.any(OriginalStateData[i]==EarlyLickDelay) or np.any(OriginalStateData[i]==EarlyLickSample):
                     early_lick = 'early'
 
-            Session.Trial().insert1((int(key[0]), int(key[1]), i, OriginalEventTimestamps[i][1], OriginalStateTimestamps[i][itemindex][0]))
+            Session.Trial().insert1((int(key[0]), int(key[1]), i, OriginalStateTimestamps[i][startindex][0], OriginalStateTimestamps[i][endindex[0]]))
             
             if Reversal==1:
-                if TrialTypes[i]==0:
+                if TrialTypes[i]==1:
                     trial_instruction = 'left'
-                elif TrialTypes[i]==1:
+                elif TrialTypes[i]==0:
                     trial_instruction = 'right'
             elif Reversal==2:
-                if TrialTypes[i]==0:
+                if TrialTypes[i]==1:
                     trial_instruction = 'right'
-                elif TrialTypes[i]==1:
+                elif TrialTypes[i]==0:
                     trial_instruction = 'left'
 
             BehaviorTrial().insert1((int(key[0]), int(key[1]), i, 'audio delay', trial_instruction, early_lick, outcome))
             TrialNote().insert1((int(key[0]), int(key[1]), i, 'protocol #', str(ProtocolType)))
+            TrialEvent().insert([(int(key[0]), int(key[1]), i, 'presample', OriginalStateTimestamps[i][startindex][0], OriginalStateTimestamps[i][sampleindex[0]]-OriginalStateTimestamps[i][startindex][0]),
+            (int(key[0]), int(key[1]), i, 'go', OriginalStateTimestamps[i][responseindex][0], AnswerPeriod)])
+            for j in range(0, len(sampleindex)):
+                TrialEvent().insert1((int(key[0]), int(key[1]), i, 'sample', OriginalStateTimestamps[i][sampleindex[j]], SampleDur))
+            for j in range(0, len(delayindex)):
+                TrialEvent().insert1((int(key[0]), int(key[1]), i, 'delay', OriginalStateTimestamps[i][delayindex[j]], DelayDur))
+            for j in range(0, len(lickleft)):
+                ActionEvent().insert1((int(key[0]), int(key[1]), i, 'left lick', OriginalEventTimestamps[i][lickleft[j]]))
+            for j in range(0, len(lickright)):
+                ActionEvent().insert1((int(key[0]), int(key[1]), i, 'right lick', OriginalEventTimestamps[i][lickright[j]]))
 
 @schema 
 class TrialNoteType(dj.Lookup):
@@ -169,9 +193,9 @@ class TrialEvent(dj.Manual):
     definition = """
     -> BehaviorTrial 
     -> TrialEventType
-    trial_event_time : decimal(8, 3)   # (s) from session and trial start (depends)
+    trial_event_time : decimal(8, 4)   # (s) from trial start, not session start (depends)
     ---
-    duration : decimal(8,3)  #  (s)  
+    duration : decimal(8,4)  #  (s)  
     """
 
 @schema
@@ -190,7 +214,7 @@ class ActionEvent(dj.Manual):
     definition = """
     -> BehaviorTrial
     -> ActionEventType
-    action_event_time : decimal(8,3)  # (s) from trial or session (it depends but please figure it out)
+    action_event_time : decimal(8,4)  # (s) from trial or session (it depends but please figure it out)
     """
 
 @schema
@@ -198,7 +222,7 @@ class TrackingDevice(dj.Lookup):
     definition = """
     tracking_device  : varchar(8)  # e.g. camera, microphone
     ---
-    sampling_rate  :  decimal(9, 3)   # Hz
+    sampling_rate  :  decimal(8, 4)   # Hz
     """
 
 @schema
@@ -226,7 +250,7 @@ class Photostim(dj.Lookup):
     photo_stim :  smallint 
     ---
     -> ccf.CCF
-    duration  :  decimal(5,3)   # (s)
+    duration  :  decimal(8,4)   # (s)
     waveform  :  longblob       # (mW)
     """
 
