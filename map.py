3d2b9b2dfb0c0d1288f8341e2b960aa41fa9829f
Chris Turner <cat@199tech.net>
2017-12-15 16:25:44 -0600

    ingest.py: zap mock data, move ingest actions into map.py utility module/script.

---
+++ map.py
@@ -0,0 +1,59 @@
+#! /usr/bin/env python
+
+import os
+import sys
+import logging
+from code import interact
+
+import datajoint as dj
+
+import ephys
+import lab
+import experiment
+import ccf
+import ingest
+
+log = logging.getLogger(__name__)
+__all__ = [ephys, lab, experiment, ccf]
+[ dj ]  # NOQA flake8 
+
+
+def usage_exit():
+    print("usage: {p} [populate|shell]"
+          .format(p=os.path.basename(sys.argv[0])))
+    sys.exit(0)
+
+
+def logsetup(*args):
+    logging.basicConfig(level=logging.ERROR)
+    log.setLevel(logging.INFO)
+    log.setLevel(logging.INFO)
+    logging.getLogger('ingest').setLevel(logging.INFO)
+
+
+def populate(*args):
+    ingest.ImportedSessionFile().populate()
+    ingest.ImportedSessionFileIngest().populate()
+
+
+def shell(*args):
+    interact('map shell.\n\nschema modules:\n\n  - {m}\n'
+             .format(m='\n  - '.join(str(m.__name__) for m in __all__)),
+             local=globals())
+
+
+actions = {
+    'shell': shell,
+    'populate': populate
+}
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) < 2 or sys.argv[1] not in actions:
+        usage_exit()
+
+    logsetup()
+
+    action = sys.argv[1]
+    actions[action](sys.argv[2:])
