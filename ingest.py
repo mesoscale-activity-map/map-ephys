f3d59ff5138ece330780e20b334ba74689ba5762
Chris Turner <cat@199tech.net>
2017-12-15 11:24:31 -0600

    ingest.py: add notes concerning rig paths

--- ingest.py
+++ ingest.py
@@ -17,6 +17,13 @@ import lab
 import experiment
 
 
+# scratch for ingest paths - to be adapted/formalized for multiple rig load
+'R:\\Arduino\\Bpod_Train1\\Bpod Local\\Data\\dl7\\TW_autoTrain\\Session Data\\'
+'Z:\\MATLAB\\Bpod Local\\Data\\dl7\\TW_autoTrain\\Session Data\\'
+'S:\\MATLAB\\Bpod Local\\Data\\dl8\\TW_autoTrain\\Session Data\\'
+'S:\\MATLAB\\Bpod Local\\Data\\dl8\\TW_autoTrain\\Session Data\\'
+
+
 if 'imported_session_path' not in dj.config:
     dj.config['imported_session_path'] = 'R:\\Arduino\\Bpod_Train1\\Bpod Local\\Data\\dl7\\TW_autoTrain\\Session Data\\'
 
