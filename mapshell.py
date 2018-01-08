4eb808a231e55562dd9e87a6458d00d4b59e057f
Chris Turner <cat@199tech.net>
2018-01-08 01:27:15 -0600

    ingest.py/mapshell.py: make ephys ingest use SessionDiscovery mechanism.
    Appears OK for initial sample file - however, some electrode related
    information is now hardcoded in make() which should be corrected.

--- mapshell.py
+++ mapshell.py
@@ -35,17 +35,8 @@ def discover(*args):
 
 
 def populate(*args):
-    # ingest.RigDataFile().populate()  # populate overridden; jobs n/a & ni
     ingest.BehaviorIngest().populate()
     ingest.EphysIngest().populate()
-    #
-    # ephys.ElectrodeGroup().insert1(
-    #     (123456, 1, 1, '15131808323',
-    #      './data_all/RRig/real5ap_imec3_opt3_jrc.mat'))
-    # note: behavior / electrode group ingest a prerequsite for this to work
-    # ephys.ElectrodeGroup().make(ephys.ElectrodeGroup().fetch1(dj.key))
-    # ephys.Ephys().populate(
-    #     reserve_jobs=True, suppress_errors=True)
 
 
 def shell(*args):
