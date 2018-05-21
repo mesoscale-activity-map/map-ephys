ff72837000d76d06ed27d477aecbf18a55c198d4
Chris Turner <cat@199tech.net>
2018-05-21 15:55:38 -0500

    scripts/ingestEphys.py: path component fixes, add debug output
    - make ephys_data_path configurable
    - remove hardcoded subpath components
    - add logging information for ephys load

--- scripts/ingestEphys.py
+++ scripts/ingestEphys.py
@@ -32,7 +32,13 @@ class EphysDataPath(dj.Lookup):
     search_order:           int                     # rig search order
     """
 
-    contents = [(r'H:\\data\MAP', 0)] # Testing the JRClust output files on my computer
+    @property
+    def contents(self):
+        if 'ephys_data_paths' in dj.config:  # for local testing
+            return dj.config['ephys_data_paths']
+
+        return ((r'H:\\data\MAP', 0),)
+
 
 @schema
 class EphysIngest(dj.Imported):
@@ -63,8 +69,8 @@ class EphysIngest(dj.Imported):
         water = (lab.WaterRestriction() & {'subject_id': subject_id}).fetch1('water_restriction_number')
         file = '{h2o}ap_imec3_opt3_jrc.mat'.format(h2o=water) # current file naming format
 #        file = '{h2o}_g0_t0.imec.ap_imec3_opt3_jrc.mat'.format(h2o=water) # some older files
-        subpath = os.path.join('Spike', date, file)
-        fullpath = os.path.join(rigpath, subpath)
+        # subpath = os.path.join('Spike', date, file)
+        fullpath = os.path.join(rigpath, file)
 
         if not os.path.exists(fullpath):
             log.info('EphysIngest().make(): skipping - no file in %s'
@@ -100,8 +106,11 @@ class EphysIngest(dj.Imported):
             'electrode_group': 1,
         }
 
+        log.debug('inserting electrode group')
         ephys.ElectrodeGroup().insert1(dict(ekey, probe_part_no=15131808323))
         ephys.ElectrodeGroup().make(ekey)  # note: no locks; is dj.Manual
+
+        log.debug('extracting spike data')
 
         f = h5py.File(fullpath,'r')
         ind = np.argsort(f['S_clu']['viClu'][0]) # index sorted by cluster
@@ -113,6 +122,7 @@ class EphysIngest(dj.Imported):
         csNote_clu=f['S_clu']['csNote_clu'][0] # manual sorting note
         strs = ["all" for x in range(len(csNote_clu))] # all units are "all" by definition
         for iU in range(0, len(csNote_clu)): # read the manual curation of each unit
+            log.debug('extracting spike indicators {s}:{u}'.format(s=behavior['session'], u=iU))
             unitQ = f[csNote_clu[iU]]
             str1 = ''.join(chr(i) for i in unitQ[:])
             if str1 == 'single': # definitions in unit quality
@@ -126,6 +136,7 @@ class EphysIngest(dj.Imported):
         spike_trials = np.ones(len(spike_times)) * (len(viT_offset_file) - 1) # every spike is in the last trial
         spike_times2 = np.copy(spike_times)
         for i in range(len(viT_offset_file) - 1, 0, -1): #find the trials each unit has a spike in
+            log.debug('locating trials with spikes {s}:{t}'.format(s=behavior['session'], t=i))
             spike_trials[spike_times < viT_offset_file[i]] = i-1 # Get the trial number of each spike
             spike_times2[(spike_times >= viT_offset_file[i-1]) & (spike_times < viT_offset_file[i])] = spike_times[(spike_times >= viT_offset_file[i-1]) & (spike_times < viT_offset_file[i])] - viT_offset_file[i - 1] # subtract the viT_offset_file from each trial
         spike_times2[np.where(spike_times2 >= viT_offset_file[-1])] = spike_times[np.where(spike_times2 >= viT_offset_file[-1])] - viT_offset_file[-1] # subtract the viT_offset_file from each trial
@@ -140,14 +151,19 @@ class EphysIngest(dj.Imported):
         trialunits1 = [] # array of unit number (for ephys.Unit.UnitTrial())
         trialunits2 = [] # array of trial number
         for i in range(0,len(trialunits)): # loop through each unit
+            log.debug('aggregating trials with units {s}:{t}'.format(s=behavior['session'], t=i))
             trialunits2 = np.append(trialunits2, np.unique(trialunits[i])) # add the trials that a unit is in
             trialunits1 = np.append(trialunits1, np.zeros(len(np.unique(trialunits[i])))+i) # add the unit numbers 
-        
+
+        log.debug('inserting units for session {s}'.format(s=behavior['session']))
         ephys.Unit().insert(list(dict(ekey, unit = x, unit_uid = x, unit_quality = strs[x], spike_times = units[x], waveform = trWav_raw_clu[x][0]) for x in unit_ids)) # batch insert the units
-        
+
         file = '{h2o}_bitcode.mat'.format(h2o=water) # fetch the bitcode and realign
-        subpath = os.path.join('Spike', date, file)
-        fullpath = os.path.join(rigpath, subpath)
+        # subpath = os.path.join('Spike', date, file)
+        fullpath = os.path.join(rigpath, file)
+
+        log.debug('opening bitcode for {s} ({f})'.format(s=behavior['session'], f=fullpath))
+
         mat = spio.loadmat(fullpath, squeeze_me = True) # load the bitcode file
         bitCodeE = mat['bitCodeS'].flatten() # bitCodeS is the char variable
         trialNote = experiment.TrialNote()
@@ -160,6 +176,8 @@ class EphysIngest(dj.Imported):
         else:
             startB = 0
             startE = 0
+
+        log.debug('extracting trial unit information {s} ({f})'.format(s=behavior['session'], f=fullpath))
         
         trialunits2 = trialunits2-startB # behavior has less trials if startB is +ve, behavior has more trials if startB is -ve
         indT = np.where(trialunits2 > -1)[0] # get rid of the -ve trials
@@ -179,6 +197,7 @@ class EphysIngest(dj.Imported):
         # split units based on which trial they are in (for ephys.TrialSpikes())
         trialPerUnit = np.copy(units) # list of trial index for each unit
         for i in unit_ids: # loop through each unit, maybe this can be avoid?
+            log.debug('.. unit information {u}'.format(u=i))
             indT = np.where(trialunits[i] > -1)[0] # get rid of the -ve trials
             trialunits[i] = trialunits[i][indT]
             units[i] = units[i][indT]
@@ -189,13 +208,18 @@ class EphysIngest(dj.Imported):
             units[i] = units[i][trialidx] # sort the spike times based on the trial mapping
             units[i] = np.split(units[i], trial_ids_diff) # separate the spike_times based on trials
             trialPerUnit[i] = np.arange(0, len(trial_ids_diff)+1, dtype = int) # list of trial index
-        
+
+
+        log.debug('inserting UnitTrial information')
         ephys.Unit.UnitTrial().insert(list(dict(ekey, unit = trialunits1[x], trial = trialunits2[x]) for x in range(0, len(trialunits2)))) # batch insert the TrialUnit (key, unit, trial)
+        log.debug('inserting UnitSpike information')
         ephys.Unit.UnitSpike().insert(list(dict(ekey, unit = cluster_ids[x]-1, spike_time = spike_times2[x], electrode = viSite_spk[x], trial = spike_trials[x]) for x in range(0, len(spike_times2))), skip_duplicates=True) # batch insert the Spikes (key, unit, spike_time, electrode, trial)
 
         # TODO: 2D batch insert
         pdb.set_trace()
+        log.debug('inserting TrialSpike information')
         ephys.TrialSpikes().insert(list(dict(ekey, unit = x[0], trial = trialunits2[x[1]], spike_times = units[x[0]][x[1]]) for x in zip(unit_ids, trialPerUnit))) # batch insert TrialSpikes
 
+        log.debug('inserting file load information')
         self.insert1(key)
         EphysIngest.EphysFile().insert1(dict(key, ephys_file=subpath))
