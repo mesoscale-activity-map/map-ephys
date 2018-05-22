75fe34f36f41d084ee5ddf49249241f332dedd7d
Chris Turner <cat@199tech.net>
2018-05-22 11:21:24 -0500

    scripts/mapshell.py: remove old 'discover', add newlines btw functions.

--- scripts/mapshell.py
+++ scripts/mapshell.py
@@ -22,10 +22,12 @@ __all__ = [ephys, lab, experiment, ccf, ingestBehavior, ingestEphys]
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
+
 def usage_exit():
     print("usage: {p} [discover|populateB|populateE|shell]"
           .format(p=os.path.basename(sys.argv[0])))
     sys.exit(0)
+
 
 def logsetup(*args):
     logging.basicConfig(level=logging.ERROR)
@@ -34,23 +36,23 @@ def logsetup(*args):
     logging.getLogger('scripts.ingestBehavior').setLevel(logging.DEBUG)
     logging.getLogger('scripts.ingestEphys').setLevel(logging.DEBUG)
 
-def discover(*args):
-    ingestBehavior.SessionDiscovery().populate()
 
 def populateB(*args):
     ingestBehavior.BehaviorIngest().populate(display_progress=True)
 
+
 def populateE(*args):
     ingestEphys.EphysIngest().populate(display_progress=True)
+
 
 def shell(*args):
     interact('map shell.\n\nschema modules:\n\n  - {m}\n'
              .format(m='\n  - '.join(str(m.__name__) for m in __all__)),
              local=globals())
 
+
 actions = {
     'shell': shell,
-    'discover': discover,
     'populateB': populateB,
     'populateE': populateE
 }
