f9816843e2f75fae30db6643dfd7df1a581a45c5
Chris Turner <cat@199tech.net>
2018-07-21 17:18:19 -0500

    tests: improve test_run_system_cmd test, add simple db-drop protection
    - test_run_system_cmd now also checks failure case
    - add a setup() function, currently used to prevent accidental db drops....
      db drop protection requires that the key 'map_testing_configuration'
      be set in dj.config for the tests to run. This should help to prevent
      accidental test suite runs against a production configuration from causing
      issues.

--- tests/test_mapshell.py
+++ tests/test_mapshell.py
@@ -1,14 +1,38 @@
 
-import os
+#123456789#123456789#123456789#123456789#123456789#123456789#123456789#123456789
 
+import os
+import datajoint as dj
+
+#
+# Utilities
+#
 
 def run_system_cmd(cmdstr):
     if os.system(cmdstr):
         raise Exception('command "{}" failed'.format(cmdstr))
 
 
+def setup():
+    # safety hack to prevent dropping live databasess
+    if 'map_testing_configuration' not in dj.config:
+        raise Exception("tests skipped - dj.config not testing configuration") 
+
+
+#
+# Actual Tests
+#
+
+
 def test_run_system_cmd():
     run_system_cmd('echo')
+
+    try:
+        run_system_cmd('sdajk jfakjdkljfk djaklj adkljf ka')
+    except Exception:
+        return True
+
+    raise Exception("bad command didn't yield exception")
 
 
 def test_mock():
