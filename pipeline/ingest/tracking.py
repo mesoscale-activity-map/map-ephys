63b3ecbe25fa57465df7e3e6ac0e92683d6444c5
Chris Turner <cat@199tech.net>
2019-03-05 14:55:59 -0600

    pipeline/ingest/tracking.py: WIP of tracking logic

--- pipeline/ingest/tracking.py
+++ pipeline/ingest/tracking.py
@@ -15,8 +15,14 @@ from glob import glob
 
 import datajoint as dj
 
+from pipeline import lab
+from pipeline import tracking
 from pipeline import experiment
 from pipeline.ingest import behavior as ingest_behavior
+
+from code import interact  # HACK debug
+from collections import ChainMap  # HACK debug
+from collections import defaultdict
 
 log = logging.getLogger(__name__)
 
@@ -30,6 +36,7 @@ schema = dj.schema(dj.config.get(
 class TrackingDataPath(dj.Lookup):
     # ephys data storage location(s)
     definition = """
+    -> lab.Rig
     tracking_data_path:         varchar(255)            # rig data path
     """
 
@@ -38,7 +45,7 @@ class TrackingDataPath(dj.Lookup):
         if 'tracking_data_paths' in dj.config:  # for local testing
             return dj.config['tracking_data_paths']
 
-        return [(r'H:\\data\MAP',)]
+        return [('RRig', r'H:\\data\MAP',)]
 
 
 @schema
@@ -58,5 +65,86 @@ class TrackingIngest(dj.Imported):
     def make(self, key):
         '''
         TrackingIngest .make() function
+
+        qs:
+        - really using multiple rig paths?
+        - camera number vs name/label: splitting would be better if OK
+        - tracking files: always -0000.csv?
+        - 1321914 points in one session.. sooo probably should blobify
+          how to structure?
         '''
         log.info('TrackingIngest().make(): key: {k}'.format(k=key))
+
+        h2o = (lab.WaterRestriction() & key).fetch1('water_restriction_number')
+        session = (experiment.Session() & key).fetch1()
+        trials = (experiment.SessionTrial() & session).fetch(as_dict=True)
+
+        sdate = session['session_date']
+        sdate_iso = sdate.isoformat()  # YYYY-MM-DD
+        sdate_sml = "{}{:02d}{:02d}".format(sdate.year, sdate.month, sdate.day)
+
+        # todo: split name vs position? confirm
+        devices = tracking.TrackingDevice().fetch('tracking_device')
+
+        # paths like: <root>/<h2o>/YYYY-MM-DD/tracking
+        for p in TrackingDataPath.fetch('tracking_data_path'):
+
+            print('key', key)  # subjectid sesssionno
+            print('trying {}'.format(p))
+            print('got session: {}'.format(session))
+            print('got trials: {}'.format(trials))
+
+            tpath = os.path.join(p, h2o, sdate_iso, 'tracking')
+
+            print('trying tracking path: {}'.format(tpath))
+
+            if not os.path.exists(tpath):
+                log.info('skipping {} - does not exist'.format(tpath))
+
+            # todo: for c in devices:
+            # interact('muhrepl', local=dict(ChainMap(locals(), globals())))
+            camtrial = '{}_{}_side.txt'.format(h2o, sdate_sml)
+            campath = os.path.join(tpath, camtrial)
+            if os.path.exists(campath):
+                tmap = self.load_campath(campath)
+                for t in tmap:
+                    log.info('t in tmap: {}'.format(t))
+                    # dl59_side_27-0000.csv / h2o_cname_t-0000.csv
+                    tfile = '{}_side_{}-0000.csv'.format(h2o, t)
+                    tfull = os.path.join(tpath, tfile)
+                    if os.path.exists(tfull):
+                        trk = self.load_tracking(tfull)
+                        # rekey based on tmap
+                        # insert.
+
+    def load_campath(self, campath):
+        ''' load camera position-to-trial map '''
+        log.debug("load_campath(): {}".format(campath))
+        with open(campath, 'r') as f:
+            return {int(k): int(v) for i in f
+                    for k, v in (i.strip().split('\t'),)}
+
+    def load_tracking(self, trkpath):
+        log.info('load_tracking() {}'.format(trkpath))
+        '''
+        file format:
+        scorer,DeepCut_resnet50_licking-sideAug10shuffle1_1030000,DeepCut_resnet50_licking-sideAug10shuffle1_1030000,DeepCut_resnet50_licking-sideAug10shuffle1_1030000,DeepCut_resnet50_licking-sideAug10shuffle1_1030000,DeepCut_resnet50_licking-sideAug10shuffle1_1030000,DeepCut_resnet50_licking-sideAug10shuffle1_1030000,DeepCut_resnet50_licking-sideAug10shuffle1_1030000,DeepCut_resnet50_licking-sideAug10shuffle1_1030000,DeepCut_resnet50_licking-sideAug10shuffle1_1030000
+        bodyparts,nose,nose,nose,tongue,tongue,tongue,jaw,jaw,jaw
+        coords,x,y,likelihood,x,y,likelihood,x,y,likelihood
+        0,418.48327827453613,257.231650352478,1.0,426.47182297706604,263.82502603530884,1.796432684386673e-06,226.12365770339966,395.8081398010254,1.0
+        '''
+        # res['nose']['x'] = 1; res['nose']['ts'] = 1.00, etc
+        res = defaultdict(lambda: defaultdict(list))
+
+        with open(trkpath, 'r') as f:
+            _, parts, fields = f.readline(), f.readline(), f.readline()
+            for l in f:
+                lv = l.split(',')
+                for i, v in enumerate(lv):
+                    if i == 0:
+                        ts = v
+                    else:
+                        res[parts[i]]['ts'] = ts  # todo: * Hz?
+                        res[parts[i]][fields[i]] = v
+
+        return res
