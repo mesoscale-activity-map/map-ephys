e28976156cae3ced973de19ba35644d61e9c9b9b
Chris Turner <cat@199tech.net>
2019-03-06 16:54:42 -0600

    pipeline/ingest/tracking.py: reorder ingest to use combined file/camera loop,
    other camera realted fixes.

--- pipeline/ingest/tracking.py
+++ pipeline/ingest/tracking.py
@@ -13,6 +13,7 @@ import os
 import logging
 from glob import glob
 
+import numpy as np
 import datajoint as dj
 
 from pipeline import lab
@@ -22,6 +23,8 @@ from pipeline.ingest import behavior as ingest_behavior
 
 from code import interact  # HACK debug
 from collections import ChainMap  # HACK debug
+# interact('muhrepl', local=dict(ChainMap(locals(), globals())))
+
 from collections import defaultdict
 
 log = logging.getLogger(__name__)
@@ -83,39 +86,80 @@ class TrackingIngest(dj.Imported):
         sdate_iso = sdate.isoformat()  # YYYY-MM-DD
         sdate_sml = "{}{:02d}{:02d}".format(sdate.year, sdate.month, sdate.day)
 
-        # todo: split name vs position? confirm
-        devices = tracking.TrackingDevice().fetch('tracking_device')
+        paths = TrackingDataPath.fetch(as_dict=True)
+        devices = tracking.TrackingDevice().fetch(as_dict=True)
 
         # paths like: <root>/<h2o>/YYYY-MM-DD/tracking
-        for p in TrackingDataPath.fetch('tracking_data_path'):
+        for p, d in ((p, d) for d in devices for p in paths):
+
+            tdev = d['tracking_device']
+            tpos = d['tracking_position']
+            tdat = p['tracking_data_path']
 
             print('key', key)  # subjectid sesssionno
-            print('trying {}'.format(p))
+            print('trying {}'.format(tdat))
             print('got session: {}'.format(session))
             print('got trials: {}'.format(trials))
 
-            tpath = os.path.join(p, h2o, sdate_iso, 'tracking')
+            tpath = os.path.join(tdat, h2o, sdate_iso, 'tracking')
 
             print('trying tracking path: {}'.format(tpath))
 
             if not os.path.exists(tpath):
                 log.info('skipping {} - does not exist'.format(tpath))
+                continue
 
-            # todo: for c in devices:
             # interact('muhrepl', local=dict(ChainMap(locals(), globals())))
-            camtrial = '{}_{}_side.txt'.format(h2o, sdate_sml)
+            camtrial = '{}_{}_{}.txt'.format(h2o, sdate_sml, tpos)
             campath = os.path.join(tpath, camtrial)
-            if os.path.exists(campath):
-                tmap = self.load_campath(campath)
-                for t in tmap:
-                    log.info('t in tmap: {}'.format(t))
-                    # dl59_side_27-0000.csv / h2o_cname_t-0000.csv
-                    tfile = '{}_side_{}-0000.csv'.format(h2o, t)
-                    tfull = os.path.join(tpath, tfile)
-                    if os.path.exists(tfull):
-                        trk = self.load_tracking(tfull)
-                        # rekey based on tmap
-                        # insert.
+
+            print('trying position/trial map: {}'.format(campath))
+            if not os.path.exists(campath):
+                log.info('skipping {} - does not exist'.format(tpath))
+                continue
+
+            tmap = self.load_campath(campath)
+
+            for t in tmap:
+                # ex: dl59_side_1-0000.csv / h2o_position_tn-0000.csv
+                # todo: glob -????.csv & err if nglob > 1
+                tfile = '{}_{}_{}-0000.csv'.format(h2o, tpos, t)
+                tfull = os.path.join(tpath, tfile)
+                if not os.path.exists(tfull):
+                    log.info('tracking file {} n/a'.format(tfull))
+
+                trk = self.load_tracking(tfull)
+
+                recs = {}
+                rec_base = dict(key, trial=tmap[t])
+
+                for k in trk:
+                    if k == 'samples':
+                        recs['tracking'] = {
+                            **rec_base,
+                            'tracking_device': tdev,
+                            'tracking_samples': len(trk['samples']['ts']),
+                        }
+                    else:
+                        rec = dict(rec_base)
+
+                        for attr in trk[k]:
+                            rec_key = '{}_{}'.format(k, attr)
+                            rec[rec_key] = np.array(trk[k][attr])
+
+                        recs[k] = rec
+
+                tracking.Tracking.insert1(
+                    recs['tracking'], allow_direct_insert=True)
+
+                tracking.Tracking.NoseTracking.insert1(
+                    recs['nose'], allow_direct_insert=True)
+
+                tracking.Tracking.TongueTracking.insert1(
+                    recs['tongue'], allow_direct_insert=True)
+
+                tracking.Tracking.JawTracking.insert1(
+                    recs['jaw'], allow_direct_insert=True)
 
     def load_campath(self, campath):
         ''' load camera position-to-trial map '''
@@ -127,24 +171,30 @@ class TrackingIngest(dj.Imported):
     def load_tracking(self, trkpath):
         log.info('load_tracking() {}'.format(trkpath))
         '''
-        file format:
+        load actual tracking data. example format:
+
         scorer,DeepCut_resnet50_licking-sideAug10shuffle1_1030000,DeepCut_resnet50_licking-sideAug10shuffle1_1030000,DeepCut_resnet50_licking-sideAug10shuffle1_1030000,DeepCut_resnet50_licking-sideAug10shuffle1_1030000,DeepCut_resnet50_licking-sideAug10shuffle1_1030000,DeepCut_resnet50_licking-sideAug10shuffle1_1030000,DeepCut_resnet50_licking-sideAug10shuffle1_1030000,DeepCut_resnet50_licking-sideAug10shuffle1_1030000,DeepCut_resnet50_licking-sideAug10shuffle1_1030000
         bodyparts,nose,nose,nose,tongue,tongue,tongue,jaw,jaw,jaw
         coords,x,y,likelihood,x,y,likelihood,x,y,likelihood
         0,418.48327827453613,257.231650352478,1.0,426.47182297706604,263.82502603530884,1.796432684386673e-06,226.12365770339966,395.8081398010254,1.0
         '''
-        # res['nose']['x'] = 1; res['nose']['ts'] = 1.00, etc
+
+        # builds result of: {'feature': {'attr': val}}
         res = defaultdict(lambda: defaultdict(list))
 
         with open(trkpath, 'r') as f:
-            _, parts, fields = f.readline(), f.readline(), f.readline()
+            f.readline()  # discard 1st line
+            parts, fields = f.readline(), f.readline()
+            parts = parts.rstrip().split(',')
+            fields = fields.rstrip().split(',')
+
             for l in f:
-                lv = l.split(',')
+                lv = l.rstrip().split(',')
                 for i, v in enumerate(lv):
+                    v = float(v)
                     if i == 0:
-                        ts = v
+                        res['samples']['ts'].append(v)  # todo: * Hz?
                     else:
-                        res[parts[i]]['ts'] = ts  # todo: * Hz?
-                        res[parts[i]][fields[i]] = v
+                        res[parts[i]][fields[i]].append(v)
 
         return res
