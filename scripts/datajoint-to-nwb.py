40d5e7f6ae1172b2783060ec2a98fa37c8bf80f0
Thinh Nguyen <37779736+ttngu207@users.noreply.github.com>
2019-05-13 10:47:22 -0400

    to nwb

---
+++ scripts/datajoint-to-nwb.py
@@ -0,0 +1,10 @@
+import os
+
+import json
+import uuid
+
+from uuid import UUID
+from datetime import date, datetime
+
+import numpy as np
+from pipeline import lab, publication, experiment, ephys, psth
