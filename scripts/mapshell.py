4c1c580ec0e3fbaa173c74b76670c224cad78bed
Chris Turner <cat@199tech.net>
2019-06-17 10:10:42 -0500

    scripts/mapshell.py: use MAP_LOGLEVEL env var to set log level

--- scripts/mapshell.py
+++ scripts/mapshell.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 # map-ephys interative shell
 
+import os
 import sys
 import pipeline.shell as shell
 
@@ -10,7 +11,7 @@ if __name__ == '__main__':
     if len(sys.argv) < 2 or sys.argv[1] not in shell.actions:
         shell.usage_exit()
 
-    shell.logsetup()
+    shell.logsetup(os.envirion.get('MAP_LOGLEVEL', 'INFO'))
 
     action = sys.argv[1]
     shell.actions[action](sys.argv[2:])
