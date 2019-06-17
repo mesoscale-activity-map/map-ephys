982a379f8617c83788952ac54d1b6743e1180ad7
Chris Turner <cat@199tech.net>
2019-06-17 10:28:45 -0500

    scripts/mapshell.py: fix shell log environment var

--- scripts/mapshell.py
+++ scripts/mapshell.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     if len(sys.argv) < 2 or sys.argv[1] not in shell.actions:
         shell.usage_exit()
 
-    shell.logsetup(os.envirion.get('MAP_LOGLEVEL', 'INFO'))
+    shell.logsetup(os.environ.get('MAP_LOGLEVEL', 'INFO'))
 
     action = sys.argv[1]
     shell.actions[action](sys.argv[2:])
