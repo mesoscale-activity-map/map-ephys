fix psth-diff mismatch - normalization rework

pipeline/plot/unit_characteristic_plot.py
@@ -41,7 +41,7 @@ def plot_clustering_quality(probe_insert_key):
 
 def plot_unit_characteristic(probe_insert_key, axs=None):
     amp, snr, spk_times, x, y, insertion_depth = (ephys.Unit * ephys.ProbeInsertion.InsertionLocation
-                                                  & probe_insert_key & 'unit_quality = "good"').fetch(
+                                                  & probe_insert_key & 'unit_quality != "all"').fetch(
         'unit_amp', 'unit_snr', 'spike_times', 'unit_posx', 'unit_posy', 'dv_location')
 
     spk_rate = np.array(list(_compute_spike_rate(spk) for spk in spk_times))
@@ -150,7 +150,7 @@ def plot_unit_bilateral_photostim_effect(probe_insert_key, axs=None):
     else:
         stim_dur = stim_durs[0] if len(stim_durs) == 1 and stim_durs[0] else default_stim_dur
 
-    units = ephys.Unit & probe_insert_key & 'unit_quality = "good"'
+    units = ephys.Unit & probe_insert_key & 'unit_quality != "all"'
 
     metrics = pd.DataFrame(columns=['unit', 'x', 'y', 'frate_change'])
 
@@ -204,8 +204,6 @@ def plot_stacked_contra_ipsi_psth(probe_insert_key, axs=None):
                      & 'period in ("sample", "delay", "response")').fetch(
                          'period_start')
 
-    good_unit = ephys.Unit & {'unit_quality': 'good'}
-
     hemi = (ephys.ProbeInsertion.InsertionLocation
             * experiment.BrainLocation & probe_insert_key).fetch1('hemisphere')
 
@@ -223,28 +221,21 @@ def plot_stacked_contra_ipsi_psth(probe_insert_key, axs=None):
     sel_c = (ephys.Unit * psth.UnitSelectivity
              & 'unit_selectivity = "contra-selective"' & probe_insert_key)
 
-    # psth_?s_?t: psth over selectivty type by specific trials
-    psth_is_it = (((psth.UnitPsth & conds_i)  # ipsi selective ipsi trials
-                   * ephys.Unit.proj('unit_posx', 'unit_posy'))
-                  & good_unit.proj() & sel_i.proj()).fetch(
-                      order_by='unit_posy desc')
+    # ipsi selective ipsi trials
+    psth_is_it = (psth.UnitPsth * sel_i.proj('unit_posy') & conds_i).fetch(order_by='unit_posy desc')
 
-    psth_is_ct = (((psth.UnitPsth & conds_c)  # ipsi selective contra trials
-                   * ephys.Unit.proj('unit_posx', 'unit_posy'))
-                  & good_unit.proj() & sel_i.proj()).fetch(
-                      order_by='unit_posy desc')
+    # ipsi selective contra trials
+    psth_is_ct = (psth.UnitPsth * sel_i.proj('unit_posy') & conds_c).fetch(order_by='unit_posy desc')
 
-    psth_cs_ct = (((psth.UnitPsth & conds_c)  # contra selective contra trials
-                   * ephys.Unit.proj('unit_posx', 'unit_posy'))
-                  & good_unit.proj() & sel_c.proj()).fetch(
-                      order_by='unit_posy desc')
+    # contra selective contra trials
+    psth_cs_ct = (psth.UnitPsth * sel_c.proj('unit_posy') & conds_c).fetch(order_by='unit_posy desc')
 
-    psth_cs_it = (((psth.UnitPsth & conds_i)  # contra selective ipsi trials
-                   * ephys.Unit.proj('unit_posx', 'unit_posy'))
-                  & good_unit.proj() & sel_c.proj()).fetch(
-                      order_by='unit_posy desc')
+    # contra selective ipsi trials
+    psth_cs_it = (psth.UnitPsth * sel_c.proj('unit_posy') & conds_i).fetch(order_by='unit_posy desc')
 
-    _plot_stacked_psth_diff(psth_cs_ct, psth_cs_it, ax=axs[0],
+    # always look at the difference in ipsi-trial subtracting contra-trial
+
+    _plot_stacked_psth_diff(psth_cs_it, psth_cs_ct, ax=axs[0],
                             vlines=period_starts)
 
     axs[0].set_title('Contra-selective Units')
@@ -252,7 +243,7 @@ def plot_stacked_contra_ipsi_psth(probe_insert_key, axs=None):
     axs[0].set_xlabel('Time to go (s)')
 
     _plot_stacked_psth_diff(psth_is_it, psth_is_ct, ax=axs[1],
-                            vlines=period_starts, flip=True)
+                            vlines=period_starts)
 
     axs[1].set_title('Ipsi-selective Units')
     axs[1].set_ylabel('Unit (by depth)')
@@ -269,7 +260,7 @@ def plot_avg_contra_ipsi_psth(probe_insert_key, axs=None):
                      & 'period in ("sample", "delay", "response")').fetch(
                          'period_start')
 
-    good_unit = ephys.Unit & {'unit_quality': 'good'}
+    good_unit = ephys.Unit & 'unit_quality != "all"'
 
     hemi = (ephys.ProbeInsertion.InsertionLocation
             * experiment.BrainLocation & probe_insert_key).fetch1('hemisphere')
@@ -506,14 +497,14 @@ def _plot_stacked_psth_diff(psth_a, psth_b, vlines=[], ax=None, flip=False):
     a_data = np.array([r[0] for r in psth_a['unit_psth']])
     b_data = np.array([r[0] for r in psth_b['unit_psth']])
 
-    # scale per-unit psth's
-    a_data = np.array([_movmean(i/np.abs(i).max()) for i in a_data])
-    b_data = np.array([_movmean(i/np.abs(i).max()) for i in b_data])
+    result = a_data - b_data
+    result = result / np.repeat(result.max(axis=1)[:, None], 149, axis=1)
 
-    if flip:
-        result = (a_data - b_data) * -1
-    else:
-        result = a_data - b_data
+    # moving average
+    result = np.array([_movmean(i) for i in result])
+
+    # flip
+    result = result * -1 if flip else result
 
     if ax is None:
         fig, ax = plt.subplots(1, 1)

pipeline/psth.py
@@ -343,7 +343,7 @@ class PeriodSelectivity(dj.Computed):
 
     alpha = 0.05  # default alpha value
 
-    key_source = experiment.Period * (ephys.Unit & 'unit_quality = "good"')
+    key_source = experiment.Period * (ephys.Unit & 'unit_quality != "all"')
 
     def make(self, key):
         '''
@@ -372,7 +372,7 @@ class PeriodSelectivity(dj.Computed):
         lr = ['left', 'right']
         behav = (experiment.BehaviorTrial & spikes_q.proj()).fetch(
             order_by='trial asc')
-        behav_lr = {k: np.where(behav['trial_instruction'] == k) for k in lr}
+        behav_lr = {k: np.where(behav['trial_instruction'] == k)[0] for k in lr}
 
         if egpos['hemisphere'] == 'left':
             behav_i = behav_lr['left']
@@ -389,7 +389,7 @@ class PeriodSelectivity(dj.Computed):
                       for st, pad in zip(spikes['spike_times'],
                                          repeat([math.nan]*ydim))]))
 
-        # with which to calculate the selctivity over the given period
+        # with which to calculate the selectivity over the given period
         period = (experiment.Period & key).fetch1()
 
         # by determining the period boundaries,
@@ -408,7 +408,7 @@ class PeriodSelectivity(dj.Computed):
         # and testing for selectivity.
         freq_i = freq[behav_i]
         freq_c = freq[behav_c]
-        t_stat, pval = sc_stats.ttest_ind(freq_i, freq_c, equal_var=False)
+        t_stat, pval = sc_stats.ttest_ind(freq_i, freq_c, equal_var=True)
 
         freq_i_m = np.average(freq_i)
         freq_c_m = np.average(freq_c)
@@ -441,7 +441,12 @@ class UnitSelectivity(dj.Computed):
     -> Selectivity.proj(unit_selectivity='selectivity')
     """
 
-    key_source = ephys.Unit & PeriodSelectivity
+    # Unit Selectivity is computed only for units
+    # that has PeriodSelectivity computed for "sample" and "delay" and "response"
+    key_source = (ephys.Unit
+                  & (PeriodSelectivity & 'period = "sample"')
+                  & (PeriodSelectivity & 'period = "delay"')
+                  & (PeriodSelectivity & 'period = "response"'))
 
     def make(self, key):
         '''
@@ -460,40 +465,40 @@ class UnitSelectivity(dj.Computed):
                 return
 
         # fetch region selectivity,
-        sel = (PeriodSelectivity & key
-               & "p_value <= '{}'".format(PeriodSelectivity.alpha)).fetch(
-                   as_dict=True)
+        sels = (PeriodSelectivity & key).fetch('period_selectivity')
 
-        if not sel:
+        assert len(sels) == 3  # one more sanity check, making sure we have all 3 periods
+
+        if (sels == 'non-selective').all():
             log.debug('... no UnitSelectivity for unit')
+            self.insert1({**key, 'unit_selectivity': 'non-selective'})
             return
 
         # left/right spikes,
-        trials_l, spikes_l = ((ephys.TrialSpikes & key)
-                              & (experiment.BehaviorTrial()
+        left_trial_spikes = ((ephys.TrialSpikes & key)
+                             & (experiment.BehaviorTrial()
                                  & {'task': 'audio delay'}
                                  & {'early_lick': 'no early'}
                                  & {'outcome': 'hit'}
                                  & {'trial_instruction': 'left'})
-                              - experiment.PhotostimEvent).fetch(
-                                  'trial', 'spike_times')
+                             - experiment.PhotostimEvent).fetch('spike_times')
 
-        trials_r, spikes_r = ((ephys.TrialSpikes & key)
+        right_trial_spikes = ((ephys.TrialSpikes & key)
                               & (experiment.BehaviorTrial()
                                  & {'task': 'audio delay'}
                                  & {'early_lick': 'no early'}
                                  & {'outcome': 'hit'}
                                  & {'trial_instruction': 'right'})
-                              - experiment.PhotostimEvent).fetch(
-                                  'trial', 'spike_times')
+                              - experiment.PhotostimEvent).fetch('spike_times')
 
         # compute their average firing rate,
-        dur = experiment.Period.trial_duration
+        period_starts, period_ends = (experiment.Period & key).fetch('period_start', 'period_end')
+        dur = sum(period_ends - period_starts)
 
-        freq_l = (len(np.concatenate(spikes_l))
-                  / (len(np.unique(trials_l)) * dur))
-        freq_r = (len(np.concatenate(spikes_r))
-                  / (len(np.unique(trials_r)) * dur))
+        freq_l = (len(np.concatenate(left_trial_spikes))
+                  / (len(left_trial_spikes) * dur))
+        freq_r = (len(np.concatenate(right_trial_spikes))
+                  / (len(right_trial_spikes) * dur))
 
         # and determine their ipsi/contra preference via frequency.
         if egpos['hemisphere'] == 'left':