1723903fedd366740680f1014be32c1631b4f707
Chris Turner <cat@199tech.net>
2019-06-24 19:01:55 -0500

    pipeline/{histology,ingest/histology.py}: add probe track ingest

--- pipeline/histology.py
+++ pipeline/histology.py
@@ -2,11 +2,9 @@ import datajoint as dj
 
 from . import lab, experiment, ccf, ephys
 from . import get_schema_name
-
-import numpy as np
+[lab, experiment, ccf, ephys]  # schema imports only
 
 schema = dj.schema(get_schema_name('histology'))
-
 
 
 @schema
@@ -37,9 +35,8 @@ class RawToCCFTransformation(dj.Imported):
     class Landmark(dj.Part):
         definition = """
         -> master
-        landmark_id: int
+        landmark_name='': varchar(8)
         ---
-        landmark_name='': varchar(32)
         raw_x: float  # (um)
         raw_y: float  # (um)
         raw_z: float  # (um)
@@ -74,14 +71,23 @@ class LabeledProbeTrack(dj.Manual):
     definition = """
     -> ephys.ProbeInsertion
     ---
-    labeling_date : date # in case we labeled the track not during a recorded session we can specify the exact date here
-    dye_color  : varchar(32)
+    labeling_date=NULL:         date
+    dye_color=NULL:             varchar(32)
     """
 
     class Point(dj.Part):
         definition = """
         -> master
         -> ccf.CCF
+        """
+
+    class PointError(dj.Part):
+        definition = """
+        -> master
+        -> ccf.CCFLabel
+        x   :  int   # (um)
+        y   :  int   # (um)
+        z   :  int   # (um)
         """
 
 
@@ -97,4 +103,3 @@ class EphysCharacteristic(dj.Imported):
     firing_rate: float
     percentage_change: float
     """
-
--- pipeline/ingest/histology.py
+++ pipeline/ingest/histology.py
@@ -1,10 +1,11 @@
-import os
+
 import logging
+import pathlib
+import csv
 
 import numpy as np
 import scipy.io as scio
 import datajoint as dj
-import pathlib
 
 from pipeline import lab
 from pipeline import ephys
@@ -33,6 +34,39 @@ class HistologyIngest(dj.Imported):
         histology_file:                 varchar(255)    # rig file subpath
         """
 
+    class LandmarkFile(dj.Part):
+        definition = """
+        -> master
+        probe_insertion_number:         tinyint         # electrode group
+        landmark_file:                  varchar(255)    # rig file subpath
+        """
+
+    '''
+    @schema
+    class LabeledProbeTrack(dj.Manual):
+        definition = """
+        -> ephys.ProbeInsertion
+        ---
+        labeling_date=NULL:         date
+        dye_color=NULL:             varchar(32)
+        """
+
+        class Point(dj.Part):
+            definition = """
+            -> master
+            -> ccf.CCF
+            """
+
+        class PointError(dj.Part):
+            definition = """
+            -> master
+            -> CCFLabel
+            x   :  int   # (um)
+            y   :  int   # (um)
+            z   :  int   # (um)
+            """
+    '''
+
     def make(self, key):
         '''
         HistologyIngest .make() function
@@ -55,76 +89,146 @@ class HistologyIngest(dj.Imported):
                            * lab.ElectrodeConfig.ElectrodeGroup
                            & session).fetch('KEY')}
 
-        sz = 20   # 20um voxel size
-
         for probe in range(1, 3):
 
-            log.info('... probe {} position ingest.'.format(probe))
+            self.load_histology_probe(key, session, probe, egmap, rigpath,
+                                      subject_id, session_date, water)
 
-            if probe not in egmap:
-                log.info('... probe {} ephys N/A, skipping.'.format(probe))
-                continue
-
-            # subpaths like:
-            # directory: {h2o}/{yyyy-mm-dd}/histology
-            # file: landmarks_{h2o}_{YYYYMMDD}_{probe}_siteInfo.mat
-
-            directory = pathlib.Path(
-                water, session_date.strftime('%Y-%m-%d'), 'histology')
-
-            file = 'landmarks_{}_{}_{}_siteInfo.mat'.format(
-                water, session['session_date'].strftime('%Y%m%d'), probe)
-            subpath = directory / file
-            fullpath = rigpath / subpath
-
-            if not fullpath.exists():
-                log.info('... probe {} histology file {} N/A, skipping.'
-                         .format(probe, fullpath))
-                continue
-
-            log.info('... found probe {} histology file {}'.format(
-                probe, fullpath))
-
-            hist = scio.loadmat(
-                fullpath, struct_as_record=False, squeeze_me=True)['site']
-
-            # probe CCF 3D positions
-            pos_xyz = np.vstack([hist.pos.x, hist.pos.y, hist.pos.z]).T * sz
-
-            # probe CCF regions
-            names = hist.ont.name
-            valid = [isinstance(n, (str,)) for n in names]
-            goodn = np.where(np.array(valid))[0]
-
-            electrodes = (ephys.ProbeInsertion * lab.ElectrodeConfig.Electrode
-                          & egmap[probe]).fetch(order_by='electrode asc')
-
-            # XXX: we index pos_xyz by 'goodn' directly,
-            #   .. rather than via electrodes[goodn]['electrode']
-
-            recs = ((*l[0], ccf.CCFLabel.CCF_R3_20UM_ID, *l[1]) for l in
-                    zip(electrodes[goodn], pos_xyz[goodn]))
-
-            # ideally:
-            # ephys.ElectrodeGroup.ElectrodePosition.insert(
-            #     recs, allow_direct_insert=True)
-            # but hitting ccf coordinate issues..:
-
-            log.info('inserting channel ccf position')
-            ephys.ElectrodeCCFPosition.insert1(
-                egmap[probe], ignore_extra_fields=True)
-
-            for r in recs:
-                log.debug('... adding probe/position: {}'.format(r))
-                try:
-                    histology.ElectrodeCCFPosition.ElectrodePosition.insert1(
-                        r, ignore_extra_fields=True, allow_direct_insert=True)
-                except Exception as e:  # XXX: no way to be more precise in dj
-                    log.warning('... ERROR!'.format(repr(e)))
-                    ephys.ElectrodeCCFPosition.ElectrodePositionError.insert1(
-                        r, ignore_extra_fields=True, allow_direct_insert=True)
-
-            log.info('... ok.')
+            self.load_histology_track(key, session, probe, egmap, rigpath,
+                                      subject_id, session_date, water)
 
         log.info('HistologyIngest().make(): {} complete.'.format(key))
         self.insert1(key)
+
+    def load_histology_probe(self, key, session, probe, egmap, rigpath,
+                             subject_id, session_date, water):
+
+        sz = 20   # 20um voxel size
+
+        log.info('... probe {} position ingest.'.format(probe))
+
+        if probe not in egmap:
+            msg = '... probe {} ephys N/A, skipping.'.format(probe)
+            log.info(msg)
+            raise StopIteration(msg)
+
+        # subpaths like:
+        # directory: {h2o}/{yyyy-mm-dd}/histology
+        # file: landmarks_{h2o}_{YYYYMMDD}_{probe}_siteInfo.mat
+
+        directory = pathlib.Path(
+            water, session_date.strftime('%Y-%m-%d'), 'histology')
+
+        file = 'landmarks_{}_{}_{}_siteInfo.mat'.format(
+            water, session['session_date'].strftime('%Y%m%d'), probe)
+        subpath = directory / file
+        fullpath = rigpath / subpath
+
+        if not fullpath.exists():
+            msg = '... probe {} histology file {} N/A, skipping.'.format(
+                probe, fullpath)
+            log.info(msg)
+            raise StopIteration(msg)
+
+        log.info('... found probe {} histology file {}'.format(
+            probe, fullpath))
+
+        hist = scio.loadmat(
+            fullpath, struct_as_record=False, squeeze_me=True)['site']
+
+        # probe CCF 3D positions
+        pos_xyz = np.vstack([hist.pos.x, hist.pos.y, hist.pos.z]).T * sz
+
+        # probe CCF regions
+        names = hist.ont.name
+        valid = [isinstance(n, (str,)) for n in names]
+        goodn = np.where(np.array(valid))[0]
+
+        electrodes = (ephys.ProbeInsertion * lab.ElectrodeConfig.Electrode
+                      & egmap[probe]).fetch(order_by='electrode asc')
+
+        # XXX: we index pos_xyz by 'goodn' directly,
+        #   .. rather than via electrodes[goodn]['electrode']
+
+        recs = ((*l[0], ccf.CCFLabel.CCF_R3_20UM_ID, *l[1]) for l in
+                zip(electrodes[goodn], pos_xyz[goodn]))
+
+        # ideally:
+        # ephys.ElectrodeGroup.ElectrodePosition.insert(
+        #     recs, allow_direct_insert=True)
+        # but hitting ccf coordinate issues..:
+
+        log.info('inserting channel ccf position')
+        ephys.ElectrodeCCFPosition.insert1(
+            egmap[probe], ignore_extra_fields=True)
+
+        for r in recs:
+            log.debug('... adding probe/position: {}'.format(r))
+            try:
+                histology.ElectrodeCCFPosition.ElectrodePosition.insert1(
+                    r, ignore_extra_fields=True, allow_direct_insert=True)
+            except Exception as e:  # XXX: no way to be more precise in dj
+                log.warning('... ERROR!'.format(repr(e)))
+                ephys.ElectrodeCCFPosition.ElectrodePositionError.insert1(
+                    r, ignore_extra_fields=True, allow_direct_insert=True)
+
+        log.info('... ok.')
+
+    def load_histology_track(key, session, probe, egmap, rigpath, subject_id,
+                             session_date, water):
+
+        conv = (('landmark_id', str), ('warp', bool),
+                ('raw_x', float), ('raw_y', float), ('raw_z', float),
+                ('x', float), ('y', float), ('z', float))
+
+        directory = pathlib.Path(
+            water, session_date.strftime('%Y-%m-%d'), 'histology')
+
+        file = 'landmarks_{}_{}_{}_siteInfo.csv'.format(
+            water, session['session_date'].strftime('%Y%m%d'), probe)
+        subpath = directory / file
+        fullpath = rigpath / subpath
+
+        recs = []
+        with open(fullpath, newline='') as f:
+            rdr = csv.reader(f)
+            for row in rdr:
+                assert len(row) == 8
+                rec = {c[0]: c[1](d) for c, d in zip(conv, row)}
+                recs.append(rec)
+
+        # Raw -> CCF Transformation
+        # XXX: we have 1x record per subject, but session copies of data..?
+        if not (histology.RawToCCFTransformation
+                & {'subject_id': subject_id}).fetch(limit=1):
+
+            log.info('... adding raw to ccf {}')
+            histology.RawToCCFTransformation.insert1(
+                key, allow_direct_insert=True)
+
+            for rec in (r for r in recs if recs['warp'] is True):
+
+                histology.RawToCCFTransformation.insert1(
+                    {'subject_id': subject_id, **rec},
+                    allow_direct_insert=True)
+
+        # LabeledProbeTrack
+        top = {**egmap[probe], 'labeling_date': None, 'dye_color': None}
+
+        histology.LabeledProbeTrack.insert1(
+            top, ignore_extra_fields=True, allow_direct_insert=True)
+
+        for rec in (r for r in recs if recs['warp'] is False):
+
+            dat = {**top, **rec}
+
+            try:
+
+                histology.LabeledProbeTrack.Point.insert1(
+                    dat, ignore_extra_fields=True, allow_direct_insert=True)
+
+            except Exception as e:  # XXX: too loose
+
+                log.error('LabeledProbeTrack.Point error: {}'.format(repr(e)))
+                histology.LabeledProbeTrack.PointError.insert1(
+                    dat, ignore_extra_fields=True, allow_direct_insert=True)
