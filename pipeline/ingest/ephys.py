4d924f3507f735c50eecd58314144df57c06ed18
Chris Turner <cat@199tech.net>
2019-08-13 14:40:06 -0500

    pipeline/ingest/ephys.py: ingest rework 1/N: ingest reimplimentation, partial modularizing

--- pipeline/ingest/ephys.py
+++ pipeline/ingest/ephys.py
@@ -3,7 +3,9 @@
 import os
 import logging
 import pathlib
+
 from glob import glob
+from itertools import repeat
 
 import scipy.io as spio
 import h5py
@@ -28,9 +30,9 @@ log = logging.getLogger(__name__)
 class EphysDataPath(dj.Lookup):
     # ephys data storage location(s)
     definition = """
-    data_path:              varchar(255)           # rig data path  
+    data_path:              varchar(255)                # rig data path
     ---
-    search_order:           int                     # rig search order
+    search_order:           int                         # rig search order
     """
 
     @property
@@ -53,13 +55,356 @@ class EphysIngest(dj.Imported):
         ''' files in rig-specific storage '''
         definition = """
         -> EphysIngest
-        probe_insertion_number:        tinyint                 # electrode_group
-        ephys_file:             varchar(255)            # rig file subpath
+        probe_insertion_number:         tinyint         # electrode_group
+        ephys_file:                     varchar(255)    # rig file subpath
         """
 
     def make(self, key):
         '''
         Ephys .make() function
+        '''
+
+        log.info('EphysIngest().make(): key: {k}'.format(k=key))
+
+        #
+        # Find corresponding BehaviorIngest
+        #
+        # ... we are keying times, sessions, etc from behavior ingest;
+        # so lookup behavior ingest for session id, quit with warning otherwise
+        #
+
+        try:
+            behavior = (behavior_ingest.BehaviorIngest() & key).fetch1()
+        except dj.DataJointError:
+            log.warning('EphysIngest().make(): skip - behavior ingest error')
+            return
+
+        log.info('behavior for ephys: {b}'.format(b=behavior))
+
+        #
+        # Find Ephys Recording
+        #
+        key = (experiment.Session & key).fetch1()
+        sinfo = ((lab.WaterRestriction()
+                  * lab.Subject().proj()
+                  * experiment.Session()) & key).fetch1()
+
+        rigpath = EphysDataPath().fetch1('data_path')
+        h2o = sinfo['water_restriction_number']
+        date = key['session_date'].strftime('%Y-%m-%d')
+
+        dpath = pathlib.Path(rigpath, h2o, date)
+        dglob = '[0-9]/{}'
+
+        v3spec = '{}_g0_*.imec.ap_imec3_opt3_jrc.mat'.format(h2o)
+        v3files = list(dpath.glob(dglob.format(v3spec)))
+
+        v4spec = '{}_g0_*.imec.ap_imec3_opt3_v4.mat'.format(h2o)  # TODO v4ify
+        v4files = list(dpath.glob(dglob.format(v4spec)))
+
+        if v3files and v4files:
+            log.warning(
+                'found v3files ({}) and v4files ({}). Skipping.'.format(
+                    v3files, v4files))
+            return
+
+        if v3files:
+            files = v3files
+            loader = self._load_v3
+
+        if v4files:
+            files = v4files
+            loader = self._load_v4
+
+        for f in files:
+            data = loader(sinfo, rigpath, dpath, f.relative_to(dpath))
+            # print('loading data ' + data)
+
+    def _gen_probe_insert(self, sinfo, probe):
+        '''
+        generate probe insertion for session / probe
+
+        Arguments:
+
+          - sinfo: lab.WaterRestriction * lab.Subject * experiment.Session
+          - probe: probe id
+
+        '''
+
+        ekey = {
+            'subject_id': sinfo['subject_id'],
+            'session': sinfo['session'],
+            'insertion_number': probe
+        }
+
+        part_no = '15131808323'  # probe model hard-coded here
+        ec_name = 'npx_first384'
+        ec = {'probe': part_no, 'electrode_config_name': ec_name}
+
+        # create hardcoded 1:384 ElectrodeConfig if needed
+        if not (lab.ElectrodeConfig & ec):
+
+            log.info('.. creating lab.ElectrodeConfig')
+
+            eg = {'probe': part_no, 'electrode_group': 0}
+
+            eg_member = [{'electrode': e} for e in range(1, 385)]
+
+            ec_hash = dict_to_hash(
+                {**eg, **{str(k): v for k, v in enumerate(eg_member)}})
+
+            lab.ElectrodeConfig.insert1(
+                {**ec, 'electrode_config_hash': ec_hash})
+
+            lab.ElectrodeConfig.ElectrodeGroup.insert1(
+                {**eg, 'electrode_config_name': ec_name})
+
+            lab.ElectrodeConfig.Electrode.insert(
+                {**eg, **m, 'electrode_config_name': ec_name}
+                for m in eg_member)
+
+        # add probe insertion
+        log.info('.. creating probe insertion')
+
+        ephys.ProbeInsertion.insert1(
+            {**ekey, 'probe': part_no, 'electrode_config_name': ec_name})
+
+        ephys.ProbeInsertion.RecordingSystemSetup.insert1(
+            {**ekey, 'sampling_rate': 30000})
+
+    def _load_v3(self, sinfo, rigpath, dpath, fpath):
+        '''
+        Ephys data loader for JRClust v4 files.
+
+        Arguments:
+
+          - sinfo: lab.WaterRestriction * lab.Subject * experiment.Session
+          - dpath: expanded rig data path
+          - fpath: file path under dpath
+
+        Returns:
+          - tbd
+        '''
+
+        h2o = sinfo['water_restriction_number']
+        skey = {k: v for k, v in sinfo.items()
+                if k in experiment.Session.primary_key}
+
+        probe = fpath.parts[0]
+
+        ef_path = pathlib.Path(dpath, fpath)
+        bf_path = pathlib.Path(dpath, probe, '{}_bitcode.mat'.format(h2o))
+
+        log.info('.. jrclust v3 data load:')
+        log.info('.... sinfo: {}'.format(sinfo))
+        log.info('.... probe: {}'.format(probe))
+
+        log.info('.... loading ef_path: {}'.format(str(ef_path)))
+        ef = h5py.File(str(pathlib.Path(dpath, fpath)))  # ephys file
+
+        log.info('.... loading bf_path: {}'.format(str(bf_path)))
+        bf = spio.loadmat(pathlib.Path(
+            dpath, probe, '{}_bitcode.mat'.format(h2o)))  # bitcode file
+
+        # extract unit data
+
+        hz = ef['P']['sRateHz'][0][0]                   # sampling rate
+
+        spikes = ef['viTime_spk'][0]                    # spike times
+        spike_sites = ef['viSite_spk'][0]               # spike electrode
+        units = ef['S_clu']['viClu'][0]                 # spike:unit id
+        unit_wav = ef['S_clu']['trWav_raw_clu']         # waveform
+        unit_notes = ef['S_clu']['csNote_clu'][0]       # curation notes
+        unit_xpos = ef['S_clu']['vrPosX_clu'][0]        # x position
+        unit_ypos = ef['S_clu']['vrPosY_clu'][0]        # y position
+        unit_amp = ef['S_clu']['vrVpp_uv_clu'][0]       # amplitude
+        unit_snr = ef['S_clu']['vrSnr_clu'][0]          # signal to noise
+
+        vmax_unit_site = ef['S_clu']['viSite_clu']      # max amplitude site
+        vmax_unit_site = np.array(vmax_unit_site[:].flatten(), dtype=np.int64)
+
+        def decode_notes(fh, notes):
+            '''
+            dereference and decode unit notes, translate to local labels
+            '''
+            note_map = {'single': 'good', 'ok': 'ok', 'multi': 'multi',
+                        '\x00\x00': 'all'}  # 'all' is default / null label
+
+            return [note_map[str().join(chr(c) for c in fh[n])] for n in notes]
+
+        unit_notes = decode_notes(ef, unit_notes)
+
+        # remove noise clusters
+        units, spikes, spike_sites = (v[i] for v, i in zip(
+            (units, spikes, spike_sites), repeat((units > 0))))
+
+        # synchronize ephys data to behavior data
+        trial_start = bf['sTrig'].flatten() - 7500      # start of trials
+        trial_go = bf['goCue'].flatten()                # go cues
+
+        sync_ephys = bf['bitCodeS'].flatten()           # ephys sync codes
+        sync_behav = (experiment.TrialNote()            # behavior sync codes
+                      & {**skey, 'trial_note_type': 'bitcode'}).fetch(
+                          'trial_note', order_by='trial')
+
+        sync_behav_start = np.where(sync_behav == sync_ephys[0])[0][0]
+        sync_behav_range = sync_behav[sync_behav_start:][:len(sync_ephys)]
+
+        # Determine trial (re)numbering for ephys:
+        #
+        # - if ephys & bitcode match, determine ephys-to-behavior trial shift
+        #   when needed for different-length recordings
+        # - otherwise, use trial number correction array (bf['trialNum'])
+
+        if not np.all(np.equal(sync_ephys, sync_behav_range)):
+            try:
+                log.info('ephys/bitcode trial mismatch - attempting fix')
+                start_behav = -1
+                trials = bf['trialNum'] - start_behav
+            except IndexError:
+                raise Exception('Bitcode Mismatch')
+        else:
+            if len(sync_behav) < len(sync_ephys):
+                start_behav = np.where(sync_behav[0] == sync_ephys)[0][0]
+            elif len(sync_behav) > len(sync_ephys):
+                start_behav = - np.where(sync_ephys[0] == sync_behav)[0][0]
+            else:
+                start_behav = 0
+            trials = np.arange(len(sync_behav_range)) - start_behav
+
+        # trialize the spikes & subtract go cue
+        t, trial_spikes, trial_units = 0, [], []
+
+        while t < len(trial_start) - 1:
+
+            s0, s1 = trial_start[t], trial_start[t+1]
+
+            trial_idx = np.where((spikes > s0) & (spikes < s1))
+
+            trial_spikes.append(spikes[trial_idx] - trial_go[t])
+            trial_units.append(units[trial_idx])
+
+            t += 1
+
+        # ... including the last trial
+        trial_idx = np.where((spikes > s1))
+        trial_spikes.append(spikes[trial_idx] - trial_go[t])
+        trial_units.append(units[trial_idx])
+
+        trial_spikes = np.array(trial_spikes)
+        trial_units = np.array(trial_units)
+
+        # convert spike data to seconds
+        spikes = spikes / hz
+        trial_start = trial_start / hz
+        trial_spikes = trial_spikes / hz
+
+        # build spike arrays
+        unit_spikes = np.array([spikes[np.where(units == u)]
+                                for u in set(units)])
+
+        unit_trial_spikes = np.array(
+            [[trial_spikes[t][np.where(trial_units[t] == u)]
+              for t in range(len(trials))] for u in set(units)])
+
+        # create probe insertion records
+        self._gen_probe_insert(sinfo, probe)
+
+        # XXX: why do we have build_unit_insert previously?
+        # ... skipped here
+
+        # TODO: electrode config should be passed back above & used here
+
+        # insert Unit
+        log.info('.. ephys.Unit')
+
+        with InsertBuffer(ephys.Unit, 10, skip_duplicates=True,
+                          allow_direct_insert=True) as ib:
+
+            for i, u in enumerate(set(units)):
+
+                ib.insert1({**skey,
+                            'insertion_number': probe,
+                            'clustering_method': 'jrclust',
+                            'unit': u,
+                            'unit_uid': u,
+                            'unit_quality': unit_notes[i],
+                            'probe': '15131808323',
+                            'electrode_config_name': 'npx_first384',
+                            'electrode_group': 0,
+                            'electrode': vmax_unit_site[i],
+                            'unit_posx': unit_xpos[i],
+                            'unit_posy': unit_ypos[i],
+                            'unit_amp': unit_amp[i],
+                            'unit_snr': unit_snr[i],
+                            'spike_times': unit_spikes[i],
+                            'waveform': unit_wav[i][0]})
+
+                if ib.flush():
+                    log.debug('....', u)
+
+        # insert Unit.UnitTrial
+        log.info('.. ephys.Unit.UnitTrial')
+
+        with InsertBuffer(ephys.Unit.UnitTrial, 10000, skip_duplicates=True,
+                          allow_direct_insert=True) as ib:
+
+            for i, u in enumerate(set(units)):
+                for t in range(len(trials)):
+                    if len(unit_trial_spikes[i][t]):
+                        ib.insert1({**skey,
+                                    'insertion_number': probe,
+                                    'clustering_method': 'jrclust',
+                                    'unit': u,
+                                    'trial': trials[t]})
+                        if ib.flush():
+                            log.debug('.... (u: {}, t: {})'.format(u, t))
+
+        # insert TrialSpikes
+        log.info('.. ephys.TrialSpikes')
+        with InsertBuffer(ephys.TrialSpikes, 10000, skip_duplicates=True,
+                          allow_direct_insert=True) as ib:
+
+            for i, u in enumerate(set(units)):
+                for t in range(len(trials)):
+                    if len(unit_trial_spikes[i][t]):
+                        ib.insert1({**skey,
+                                    'insertion_number': probe,
+                                    'clustering_method': 'jrclust',
+                                    'unit': u,
+                                    'trial': trials[t],
+                                    'spike_times': unit_trial_spikes[i][t]})
+                        if ib.flush():
+                            log.debug('.... (u: {}, t: {})'.format(u, t))
+
+        log.info('.. inserting file load information')
+
+        self.insert1(skey, skip_duplicates=True)
+
+        EphysIngest.EphysFile().insert1(
+            {**skey, 'probe_insertion_number': probe,
+             'ephys_file': str(ef_path.relative_to(rigpath))})
+
+        log.info('ephys ingest for {} complete'.format(skey))
+
+        # from code import interact
+        # from collections import ChainMap
+        # interact('_load_v3', local=dict(ChainMap(locals(), globals())))
+
+
+    def _load_v4(self, sinfo, dpath, fpath):
+        '''
+        Ephys data loader for JRClust v4 files.
+        Arguments:
+          - sinfo: lab.WaterRestriction * lab.Subject * experiment.Session
+          - dpath: expanded rig data path
+          - fpath: file path under dpath
+        '''
+        pass
+
+    def make_v3(self, sinfo, key):
+        '''
+        Ephys .make() function - for loading JRClust v3 files
         '''
 
         log.info('EphysIngest().make(): key: {k}'.format(k=key))
@@ -152,7 +497,7 @@ class EphysIngest(dj.Imported):
             cluster_ids = f['S_clu']['viClu'][0]# cluster (unit) number
             trWav_raw_clu = f['S_clu']['trWav_raw_clu'] # spike waveform
     #        trWav_raw_clu1 = np.concatenate((trWav_raw_clu[0:1][:][:],trWav_raw_clu),axis=0) # add a spike waveform to cluster 0, not necessary anymore after the previous step
-            csNote_clu = f['S_clu']['csNote_clu'][0] # manual sorting note
+            csNote_clu = f['S_clu']['csNote_clu'][0] # manual sorting notew
             viSite_clu = f['S_clu']['viSite_clu'][:] # site of the unit with the largest amplitude
             vrPosX_clu = f['S_clu']['vrPosX_clu'][0] # x position of the unit
             vrPosY_clu = f['S_clu']['vrPosY_clu'][0] # y position of the unit
