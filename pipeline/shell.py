6d6493c87d9b15ae74aa166e145598f9c10c00be
Chris Turner <cat@199tech.net>
2019-10-18 15:19:20 -0500

    publication: ArchivedVideoTracking -> ArchivedTrackingVideo (rm Tracking ref)
    - allows linking/publishing raw videos without running tracking feature
      detection
    - table rename to more logically represent table contents & potentially allow
      separate tracking feature detection later

--- pipeline/shell.py
+++ pipeline/shell.py
@@ -111,7 +111,7 @@ def nuke_all():
 
 def publish(*args):
     publication.ArchivedRawEphys.populate()
-    publication.ArchivedVideoTracking.populate()
+    publication.ArchivedTrackingVideo.populate()
 
 
 def export_recording(*args):
