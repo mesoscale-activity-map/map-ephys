8fd5b7c835bf04bc849d2bc1050d1b1cb58bbfaf
Thinh Nguyen <thinh@vathes.com>
2020-03-27 15:03:52 -0500

    still export unit amp, snr, for non-qc sessions

--- pipeline/export.py
+++ pipeline/export.py
@@ -142,7 +142,7 @@ def _export_recording(insert_key, output_dir='./', filename=None, overwrite=Fals
                'behavior_report',
                'behavior_early_report', 'behavior_lick_times',
                'behavior_is_free_water', 'behavior_is_auto_water',
-               'task_trial_type', 'task_stimulation',
+               'task_trial_type', 'task_stimulation', 'trial_end_time',
                'task_sample_time', 'task_delay_time', 'task_cue_time',
                'tracking', 'histology']
 
@@ -207,10 +207,16 @@ def _export_recording(insert_key, output_dir='./', filename=None, overwrite=Fals
     # unit_snr: (Nx1)
     # ...
 
-    q_qc = ephys.Unit.proj('unit_amp', 'unit_snr') * ephys.UnitStat * ephys.ClusterMetric * ephys.WaveformMetric
+    q_qc = (ephys.Unit & insert_key).proj('unit_amp', 'unit_snr').aggr(
+        ephys.UnitStat, ..., **{n: n for n in ephys.UnitStat.heading.names if n not in ephys.UnitStat.heading.primary_key},
+        keep_all_rows=True).aggr(
+        ephys.ClusterMetric, ..., **{n: n for n in ephys.ClusterMetric.heading.names if n not in ephys.ClusterMetric.heading.primary_key},
+        keep_all_rows=True).aggr(
+        ephys.WaveformMetric, ..., **{n: n for n in ephys.WaveformMetric.heading.names if n not in ephys.WaveformMetric.heading.primary_key},
+        keep_all_rows=True)
     qc_names = [n for n in q_qc.heading.names if n not in q_qc.primary_key]
 
-    if q_qc & insert_key:
+    if q_qc:
         qc = (q_qc & insert_key).fetch(*qc_names, order_by='unit')
         qc_df = pd.DataFrame(qc).T
         qc_df.columns = qc_names
@@ -368,6 +374,19 @@ def _export_recording(insert_key, output_dir='./', filename=None, overwrite=Fals
         'go_time', 'duration', order_by='trial')
 
     edata['task_cue_time'] = np.array([_tct, _tcd]).astype(float)
+
+    print('ok.')
+
+    # trial_end_time - list of (onset, duration)
+    # -------------
+
+    print('... trial_end_time:', end='')
+
+    _tet, _ted = (experiment.BehaviorTrial & insert_key).aggr(experiment.TrialEvent & 'trial_event_type = "trialend"',
+                                                              trialend_time='trial_event_time', duration='duration').fetch(
+        'trialend_time', 'duration', order_by='trial')
+
+    edata['trial_end_time'] = np.array([_tet, _ted]).astype(float)
 
     print('ok.')
 
