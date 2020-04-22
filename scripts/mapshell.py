5434806719d2e0427652035a7e17f6c4ebfda91d
Chris Turner <cat@199tech.net>
2020-04-22 12:25:42 -0500

    shell/mapshell: fix updated dispatch

--- scripts/mapshell.py
+++ scripts/mapshell.py
@@ -19,4 +19,4 @@ if __name__ == '__main__':
                        dj.config.get('loglevel', 'INFO')))
 
     action = sys.argv[1]
-    shell.actions[action][1](*sys.argv[2:])
+    shell.actions[action][0](*sys.argv[2:])
