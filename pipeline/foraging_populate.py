aff20dae139156f90ecf20e04eb1517f95f17322
han-lab <houhan@gmail.com>
2020-09-08 19:05:01 -0400

    rename according to Thinh's convention

--- pipeline/foraging_populate.py
+++ pipeline/foraging_populate.py
@@ -1,6 +1,5 @@
 import datajoint as dj
-from pipeline import lab, get_schema_name
-from foraging_inspector import behavior_foraging
+from pipeline import lab, get_schema_name, foraging_analysis
 
 try:
     import ray
@@ -8,13 +7,13 @@ try:
     @ray.remote
     def populatemytables_core_paralel(arguments,runround):
         if runround == 1:
-            behavior_foraging.TrialReactionTime().populate(**arguments)
-            behavior_foraging.BlockStats().populate(**arguments)
-            behavior_foraging.SessionStats().populate(**arguments)
-            behavior_foraging.SessionTaskProtocol().populate(**arguments)
-            behavior_foraging.BlockFraction().populate(**arguments)
-            behavior_foraging.SessionMatching().populate(**arguments)
-            behavior_foraging.BlockEfficiency().populate(**arguments)   
+            foraging_analysis.TrialReactionTime().populate(**arguments)
+            foraging_analysis.BlockStats().populate(**arguments)
+            foraging_analysis.SessionStats().populate(**arguments)
+            foraging_analysis.SessionTaskProtocol().populate(**arguments)
+            foraging_analysis.BlockFraction().populate(**arguments)
+            foraging_analysis.SessionMatching().populate(**arguments)
+            foraging_analysis.BlockEfficiency().populate(**arguments)   
             
     use_ray = True
 except:
@@ -23,28 +22,28 @@ except:
     import multiprocessing as mp
     def populatemytables_core_paralel(arguments,runround):
         if runround == 1:
-            behavior_foraging.TrialReactionTime().populate(**arguments)
-            behavior_foraging.BlockStats().populate(**arguments)
-            behavior_foraging.SessionStats().populate(**arguments)
-            behavior_foraging.SessionTaskProtocol().populate(**arguments)
-            behavior_foraging.BlockFraction().populate(**arguments)
-            behavior_foraging.SessionMatching().populate(**arguments)
-            behavior_foraging.BlockEfficiency().populate(**arguments)   
+            foraging_analysis.TrialReactionTime().populate(**arguments)
+            foraging_analysis.BlockStats().populate(**arguments)
+            foraging_analysis.SessionStats().populate(**arguments)
+            foraging_analysis.SessionTaskProtocol().populate(**arguments)
+            foraging_analysis.BlockFraction().populate(**arguments)
+            foraging_analysis.SessionMatching().populate(**arguments)
+            foraging_analysis.BlockEfficiency().populate(**arguments)   
     
 def populatemytables_core(arguments,runround):
     if runround == 1:
-        behavior_foraging.TrialReactionTime().populate(**arguments)
-        behavior_foraging.BlockStats().populate(**arguments)
-        behavior_foraging.SessionStats().populate(**arguments)
-        behavior_foraging.SessionTaskProtocol().populate(**arguments)
-        behavior_foraging.BlockFraction().populate(**arguments)
-        behavior_foraging.SessionMatching().populate(**arguments)
-        behavior_foraging.BlockEfficiency().populate(**arguments)           
+        foraging_analysis.TrialReactionTime().populate(**arguments)
+        foraging_analysis.BlockStats().populate(**arguments)
+        foraging_analysis.SessionStats().populate(**arguments)
+        foraging_analysis.SessionTaskProtocol().populate(**arguments)
+        foraging_analysis.BlockFraction().populate(**arguments)
+        foraging_analysis.SessionMatching().populate(**arguments)
+        foraging_analysis.BlockEfficiency().populate(**arguments)           
         
 def populatemytables(paralel = True, cores = 9):
     IDs = {k: v for k, v in zip(*lab.WaterRestriction().fetch('water_restriction_number', 'subject_id'))}              
     if paralel:
-        schema = dj.schema(get_schema_name('behavior_foraging'),locals())
+        schema = dj.schema(get_schema_name('foraging_analysis'),locals())
         schema.jobs.delete()
         
         if use_ray:
