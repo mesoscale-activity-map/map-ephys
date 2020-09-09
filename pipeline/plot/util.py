18fa868917c8b4b9c17b32d59e0522c8ed291fef
han-lab <houhan@gmail.com>
2020-09-09 18:55:17 -0400

    NO-PR: remove ephys for my own playground

--- pipeline/plot/util.py
+++ pipeline/plot/util.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
-from pipeline import experiment, ephys, psth
+from pipeline import experiment
 
 
 def show_source(function):
