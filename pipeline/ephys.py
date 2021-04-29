b3272aa07266d7823a0a9f1ca5ec937ed559e01a
Chris Turner <cat@199tech.net>
2021-04-29 16:22:50 -0500

    pipeline/ephys.py: fix key_source join issue in UnitNote/UnitNoiseLabel

--- pipeline/ephys.py
+++ pipeline/ephys.py
@@ -197,7 +197,7 @@ class UnitNote(dj.Imported):
     note_value: varchar(128)
     """
 
-    key_source = ProbeInsertion & Unit
+    key_source = ProbeInsertion & Unit.proj()
 
     def make(self, key):
         pass
@@ -213,7 +213,7 @@ class UnitNoiseLabel(dj.Imported):
     noise: enum('good', 'noise')
     """
 
-    key_source = ProbeInsertion & Unit
+    key_source = ProbeInsertion & Unit.proj()
 
     def make(self, key):
         pass
