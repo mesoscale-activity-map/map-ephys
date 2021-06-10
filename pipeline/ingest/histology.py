d35403603a971e0bee74a0bcbecd38b184165ebd
Thinh Nguyen <thinh@vathes.com>
2021-06-10 17:20:03 -0500

    update histology ingest with format 2 (IBL GUI) - do labeled probetrack and handle multi shank

--- pipeline/ingest/histology.py
+++ pipeline/ingest/histology.py
@@ -103,7 +103,7 @@ class HistologyIngest(dj.Imported):
                 directory = pathlib.Path(rigpath, self.water) / '{}_{}'.format(
                     self.session_date_str, self.probe)
                 try:
-                    self.directory = next(directory.rglob('channel_locations.json')).parent
+                    self.directory = next(directory.rglob('channel_locations*.json')).parent
                     break
                 except StopIteration:
                     pass
@@ -217,106 +217,98 @@ class HistologyIngest(dj.Imported):
             return True
 
         if found['format'] == 2:
-
             # load data,
-            cloc_data = {}
-            cloc_file = found['channel_locations']
+            for channel_location_file, shank_no in zip(found['channel_location_files'],
+                                                       found['corresponding_shanks']):
 
-            log.debug('loading format 2 channels from {}'.format(cloc_file))
-            with open(cloc_file, 'r') as fh:
-                cloc_raw = json.loads(fh.read())
+                log.debug('loading format 2 channels from {}'.format(channel_location_file))
+                with open(channel_location_file, 'r') as fh:
+                    cloc_raw = json.loads(fh.read())
 
-            cloc_data['origin'] = cloc_raw['origin']
+                cloc_data = {'origin': cloc_raw['origin']}
 
-            if len(cloc_data['origin'].keys()) > 1:
-                log.error('> 1 origin region found ({}). skipping.'.format(
-                    cloc_data['origin']))
+                if len(cloc_data['origin'].keys()) > 1:
+                    log.error('More than one origin region found ({}). skipping.'.format(
+                        cloc_data['origin']))
+                    return
 
-                return
+                # ensuring channel data is sorted;
+                cloc_keymap = {int(k.split('_')[1]): k for k
+                               in cloc_raw.keys() if 'channel_' in k}
 
-            # ensuring channel data is sorted;
-            cloc_keymap = {int(k.split('_')[1]): k for k
-                           in cloc_raw.keys() if 'channel_' in k}
+                cloc_data['channels'] = np.array(
+                    [tuple(cloc_raw[cloc_keymap[k]].values()) for k in sorted(
+                        cloc_keymap.keys())],
+                    dtype=[
+                        ('x', np.float), ('y', np.float), ('z', np.float),
+                        ('axial', np.float), ('lateral', np.float),
+                        ('brain_region_id', np.int), ('brain_region', np.object)])
 
-            cloc_data['channels'] = np.array(
-                [tuple(cloc_raw[cloc_keymap[k]].values()) for k in sorted(
-                    cloc_keymap.keys())],
-                dtype=[
-                    ('x', np.float), ('y', np.float), ('z', np.float),
-                    ('axial', np.float), ('lateral', np.float),
-                    ('brain_region_id', np.int), ('brain_region', np.object)])
+                # get/scale xyz positions
+                pos_xyz_raw = np.array([cloc_data['channels'][i]
+                                        for i in ('x', 'y', 'z')]).T
 
-            # get/scale xyz positions
-            pos_xyz_raw = np.array([cloc_data['channels'][i]
-                                    for i in ('x', 'y', 'z')]).T
+                pos_origin = cloc_data['origin'][
+                    list(cloc_data['origin'].keys())[0]]
 
-            pos_origin = cloc_data['origin'][
-                list(cloc_data['origin'].keys())[0]]
+                pos_xyz = np.copy(pos_xyz_raw)
 
-            pos_xyz = np.copy(pos_xyz_raw)
+                # by adjusting xyz axes & offsetting from origin position
+                pos_xyz[:, 0] = pos_origin[0] + pos_xyz_raw[:, 0]
+                pos_xyz[:, 1] = pos_origin[2] - pos_xyz_raw[:, 2]
+                pos_xyz[:, 2] = pos_origin[1] - pos_xyz_raw[:, 1]
 
-            # by adjusting xyz axes & offsetting from origin position
-            pos_xyz[:, 0] = pos_origin[0] + pos_xyz_raw[:, 0]
-            pos_xyz[:, 1] = pos_origin[2] - pos_xyz_raw[:, 2]
-            pos_xyz[:, 2] = pos_origin[1] - pos_xyz_raw[:, 1]
+                # and quantizing to CCF voxel size;
+                pos_xyz = ccf_res * np.around(pos_xyz / ccf_res)
 
-            # and quantizing to CCF voxel size;
-            pos_xyz = ccf_res * np.around(pos_xyz / ccf_res)
+                # get recording geometry,
+                probe_electrodes = (lab.ProbeType.Electrode
+                                    & (ephys.ProbeInsertion & self.egroup)
+                                    & {'shank': shank_no}).fetch(order_by='electrode asc')
 
-            # get recording geometry,
-            probe_electrodes = (lab.ProbeType.Electrode
-                                & (ephys.ProbeInsertion
-                                   & self.egroup)).fetch(
-                                       order_by='electrode asc')
+                rec_electrodes = np.array(
+                    [cloc_data['channels']['lateral'],
+                     cloc_data['channels']['axial']]).T
 
-            rec_electrodes = np.array(
-                [cloc_data['channels']['lateral'],
-                 cloc_data['channels']['axial']]).T
+                # adjusting to boundaries, # FIXME: WHY, ISOK?
+                # also: example session was -= 11; this seems more robust.
+                rec_electrodes[:, 0] = (
+                    16 * (np.around(rec_electrodes[:, 0] / 16) - 1))
 
-            # adjusting to boundaries, # FIXME: WHY, ISOK?
-            # also: example session was -= 11; this seems more robust.
-            rec_electrodes[:, 0] = (
-                16 * (np.around(rec_electrodes[:, 0] / 16) - 1))
+                # to find corresponding electrodes,
+                elec_coord = np.array(
+                    [probe_electrodes['x_coord'], probe_electrodes['y_coord']]).T
 
-            # to find corresponding electrodes,
-            elec_coord = np.array(
-                [probe_electrodes['x_coord'], probe_electrodes['y_coord']]).T
+                elec_coord_map = {tuple(i[1]): i[0]
+                                  for i in enumerate(elec_coord)}
 
-            elec_coord_map = {tuple(i[1]): i[0]
-                              for i in enumerate(elec_coord)}
+                rec_to_elec_idx = np.array([elec_coord_map[tuple(i)]
+                                            for i in rec_electrodes])
 
-            rec_to_elec_idx = np.array([elec_coord_map[tuple(i)]
-                                        for i in rec_electrodes])
+                # and then insert the ElectrodeCCFPosition records.
+                log.debug('... adding ElectrodeCCFPosition: {}'.format(
+                    self.egroup))
 
-            # and then insert the ElectrodeCCFPosition records.
-            log.debug('... adding ElectrodeCCFPosition: {}'.format(
-                self.egroup))
+                histology.ElectrodeCCFPosition.insert1(
+                    self.egroup, ignore_extra_fields=True, skip_duplicates=True)
 
-            ElectrodeCCFPosition = histology.ElectrodeCCFPosition
-            ElectrodePosition = ElectrodeCCFPosition.ElectrodePosition
-            ElectrodePositionError = (
-                ElectrodeCCFPosition.ElectrodePositionError)
+                for z in zip(probe_electrodes[rec_to_elec_idx]['electrode'],
+                             pos_xyz[:, 0], pos_xyz[:, 1], pos_xyz[:, 2]):
 
-            ElectrodeCCFPosition.insert1(
-                self.egroup, ignore_extra_fields=True, skip_duplicates=True)
+                    z = [int(i) for i in z]  # integer CCF voxels, electrodes.
+                    rec = {**self.egroup, 'electrode': z[0],
+                           'ccf_label_id': ccf.CCFLabel.CCF_R3_20UM_ID,
+                           'ccf_x': z[1], 'ccf_y': z[2], 'ccf_z': z[3]}
+                            # via nullable: 'mri_x': 0, 'mri_y': 0, 'mri_z': 0
 
-            for z in zip(probe_electrodes[rec_to_elec_idx]['electrode'],
-                         pos_xyz[:, 0], pos_xyz[:, 1], pos_xyz[:, 2]):
+                    log.debug('...... adding ElectrodePosition: {}'.format(rec))
 
-                z = [int(i) for i in z]  # integer CCF voxels, electrodes.
-                rec = {**self.egroup, 'electrode': z[0],
-                       'ccf_label_id': ccf.CCFLabel.CCF_R3_20UM_ID,
-                       'ccf_x': z[1], 'ccf_y': z[2], 'ccf_z': z[3]}
-                        # via nullable: 'mri_x': 0, 'mri_y': 0, 'mri_z': 0
-
-                log.debug('...... adding ElectrodePosition: {}'.format(rec))
-
-                try:
-                    ElectrodePosition.insert1(rec)
-                except Exception as e:  # XXX: no way to be more precise in dj
-                    log.warning('...... ElectrodePositionError: {}'.format(
-                        repr(e)))
-                    ElectrodePositionError.insert1(rec)
+                    try:
+                        histology.ElectrodeCCFPosition.ElectrodePosition.insert1(rec)
+                    except Exception as e:  # XXX: no way to be more precise in dj
+                        log.warning('...... ElectrodePositionError: {}'.format(
+                            repr(e)))
+                        histology.ElectrodeCCFPosition.ElectrodePositionError.insert1(rec)
 
             return True
 
@@ -326,58 +318,91 @@ class HistologyIngest(dj.Imported):
 
         found = self._search_histology_files('histology_file')
 
-        trackpaths = found['histology_files']
-        shanks = found['corresponding_shanks']
+        probe_track_key = {**self.egroup, 'labeling_date': None, 'dye_color': None}
 
-        if trackpaths:
-            log.info('... found probe {} histology file(s): {}'.format(
-                self.probe, trackpaths))
-        else:
-            raise FileNotFoundError
+        histology.LabeledProbeTrack.insert1(
+            probe_track_key, ignore_extra_fields=True, allow_direct_insert=True, skip_duplicates=True)
 
-        conv = (('landmark_name', str), ('warp', lambda x: x.lower() == 'true'),
-                ('subj_x', float), ('subj_y', float), ('subj_z', float),
-                ('ccf_x', float), ('ccf_y', float), ('ccf_z', float))
+        if found['format'] == 1:
+            trackpaths = found['histology_files']
+            shanks = found['corresponding_shanks']
 
-        for trackpath, shank_no in zip(trackpaths, shanks):
-            recs = []
-            with open(trackpath.as_posix(), newline='') as f:
-                rdr = csv.reader(f)
-                for row in rdr:
-                    assert len(row) == 8
-                    rec = {c[0]: c[1](d) for c, d in zip(conv, row)}
-                    recs.append(rec)
-
-            # Subject -> CCF Transformation
-
-            top = {'subject_id': self.session['subject_id']}
-
-            if not (histology.SubjectToCCFTransformation & top).fetch(limit=1):
-
-                log.info('... adding new raw -> ccf coordinates')
-
-                histology.SubjectToCCFTransformation.insert1(
-                    top, allow_direct_insert=True)
-
-                histology.SubjectToCCFTransformation.Landmark.insert(
-                    ({**top, **rec} for rec in
-                     (r for r in recs if r['warp'] is True)),
-                    allow_direct_insert=True, ignore_extra_fields=True)
-
+            if trackpaths:
+                log.info('... found probe {} histology file(s): {}'.format(
+                    self.probe, trackpaths))
             else:
-                log.debug('... skipping raw -> ccf coordinates')
+                raise FileNotFoundError
 
-            # LabeledProbeTrack
+            conv = (('landmark_name', str), ('warp', lambda x: x.lower() == 'true'),
+                    ('subj_x', float), ('subj_y', float), ('subj_z', float),
+                    ('ccf_x', float), ('ccf_y', float), ('ccf_z', float))
 
-            top = {**self.egroup, 'labeling_date': None, 'dye_color': None}
+            for trackpath, shank_no in zip(trackpaths, shanks):
+                recs = []
+                with open(trackpath.as_posix(), newline='') as f:
+                    rdr = csv.reader(f)
+                    for row in rdr:
+                        assert len(row) == 8
+                        rec = {c[0]: c[1](d) for c, d in zip(conv, row)}
+                        recs.append(rec)
 
-            histology.LabeledProbeTrack.insert1(
-                top, ignore_extra_fields=True, allow_direct_insert=True, skip_duplicates=True)
+                # Subject -> CCF Transformation
 
-            histology.LabeledProbeTrack.Point.insert(
-                ({**top, 'order': rec_order, 'shank': shank_no, **rec} for rec_order, rec in
-                 enumerate((r for r in recs if r['warp'] is False))),
-                ignore_extra_fields=True, allow_direct_insert=True)
+                subj_key = {'subject_id': self.session['subject_id']}
+
+                if not (histology.SubjectToCCFTransformation & subj_key).fetch(limit=1):
+
+                    log.info('... adding new raw -> ccf coordinates')
+
+                    histology.SubjectToCCFTransformation.insert1(
+                        subj_key, allow_direct_insert=True)
+
+                    histology.SubjectToCCFTransformation.Landmark.insert(
+                        ({**subj_key, **rec} for rec in
+                         (r for r in recs if r['warp'] is True)),
+                        allow_direct_insert=True, ignore_extra_fields=True)
+
+                else:
+                    log.debug('... skipping raw -> ccf coordinates')
+
+                # LabeledProbeTrack
+                histology.LabeledProbeTrack.Point.insert(
+                    ({**probe_track_key, 'order': rec_order, 'shank': shank_no, **rec} for rec_order, rec in
+                     enumerate((r for r in recs if r['warp'] is False))),
+                    ignore_extra_fields=True, allow_direct_insert=True)
+
+        if found['format'] == 2:
+            for channel_location_file, xyz_picks_file, shank_no in zip(
+                    found['channel_location_files'],
+                    found['xyz_picks_files'],
+                    found['corresponding_shanks']):
+
+                log.debug('loading format 2 channels from {}'.format(channel_location_file))
+                with open(channel_location_file, 'r') as fh:
+                    channel_location = json.loads(fh.read())
+                with open(xyz_picks_file, 'r') as fh:
+                    xyz_pick = json.loads(fh.read())
+
+                if len(channel_location['origin']) > 1:
+                    log.error('More than one origin region found ({}). skipping.'.format(
+                        channel_location['origin']))
+                    return
+
+                pos_origin = next(iter(channel_location['origin'].values()))
+                xyz_pick = np.array(xyz_pick['xyz_picks'])
+
+                shank_xyz = np.copy(xyz_pick)
+                # by adjusting xyz axes & offsetting from origin position
+                shank_xyz[:, 0] = pos_origin[0] + xyz_pick[:, 0]
+                shank_xyz[:, 1] = pos_origin[2] - xyz_pick[:, 2]
+                shank_xyz[:, 2] = pos_origin[1] - xyz_pick[:, 1]
+
+                # LabeledProbeTrack
+                histology.LabeledProbeTrack.Point.insert(
+                    ({**probe_track_key, 'order': idx, 'shank': shank_no,
+                      'ccf_x': x, 'ccf_y': y, 'ccf_z': z} for idx, (x, y, z) in
+                     enumerate(shank_xyz)),
+                    ignore_extra_fields=True, allow_direct_insert=True)
 
         return True
 
@@ -429,9 +454,9 @@ class HistologyIngest(dj.Imported):
             filename_prefix = '{}_{}_{}_'.format(
                 self.water, self.session_date_str, self.probe)
 
-        format_number = 2 if (self.directory / '{}channel_locations.json'.format(filename_prefix)).exists() else 1
+        format_number = 2 if (self.directory / '{}channel_locations*.json'.format(filename_prefix)).exists() else 1
 
-        if format_number == 1 or file_type == 'histology_file':
+        if format_number == 1:
             file_format_map = {'landmark_file': '_siteInfo.mat',
                                'histology_file': '.csv'}
 
@@ -491,30 +516,38 @@ class HistologyIngest(dj.Imported):
                 'corresponding_shanks': corresponding_shanks
             }
         elif format_number == 2:
-            res = {'format': 2}
             log.debug('format 2 detected..')
 
-            new_histology_files = {
-                'channel_locations': (self.directory / '{}channel_locations.json'.format(
-                    filename_prefix)),
-                'raw_ind': (self.directory / 'channels.rawInd.npy'),
-                'channel_local_coordinates': (
-                    self.directory /
-                    '{}channels.localCoordinates.npy'.format(
-                        filename_prefix))
+            channel_locations_files = list(self.directory.glob(
+                '{}channel_locations*.json'.format(filename_prefix)))
+            xyz_picks_files = list(self.directory.glob('xyz_picks*.json'))
+
+            if len(channel_locations_files) < 1:
+                raise FileNotFoundError(
+                    'Probe {} histology file {} not found!'.format(
+                        self.probe,
+                        '{}channel_locations*.json'.format(filename_prefix)))
+            elif len(channel_locations_files) == 1:
+                corresponding_shanks = [1]
+                if len(self.shanks) != 1:
+                    raise HistologyFileError(
+                        'Only 1 file found ({}) for a {}-shank probe'.format(
+                            channel_locations_files[0].name, len(self.shanks)))
+                if 'shank' in channel_locations_files[0].stem and channel_locations_files[0].stem[-1] != 1:
+                    raise HistologyFileError('The histology file found ({}) is unexpected for this 1-shank probe')
+            else:
+                if len(channel_locations_files) != len(self.shanks):  # ensure 1 file per shank
+                    raise HistologyFileError(
+                        '{} files found for a {}-shank probe'.format(len(channel_locations_files),
+                                                                     len(self.shanks)))
+                corresponding_shanks = [int(f.stem[-1]) for f in channel_locations_files]
+
+            res = {
+                'format': 2,
+                'channel_locations_files': channel_locations_files,
+                'xyz_picks_files': xyz_picks_files,
+                'corresponding_shanks': corresponding_shanks
             }
-
-            new_histology_files_state = {i: new_histology_files[i].exists()
-                                         for i in new_histology_files}
-
-            if not all(new_histology_files_state.values()):
-                log.warning(
-                    'new format incomplete: status: {}'.format(
-                        new_histology_files_state))
-
-            for s in (s for s in new_histology_files_state
-                      if new_histology_files_state[s]):
-                res[s] = new_histology_files[s]
 
         return res
 
