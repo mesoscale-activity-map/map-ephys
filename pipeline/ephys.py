92a5d0fe75ff8dd93f586faf5d82a31ea977efad
han-lab <houhan@gmail.com>
2021-07-14 19:12:47 -0400

    reuse experiment.TrialEventType for ephys

--- pipeline/ephys.py
+++ pipeline/ephys.py
@@ -200,16 +200,6 @@ class Unit(dj.Imported):
 
 
 @schema
-class TrialEventType(dj.Lookup):
-    definition = """
-    trial_event_type  : varchar(20)  
-    """
-    contents = zip(('bitcodestart', 'go', 'choice', 'reward', 'trialend',   # Same as experiment.TrialEvent; could be used for cross-validation
-                    'bpodstart', 'zaberstep', 'zaberinposition'    # Events that are only available from NIDQ channels (camera pulses into the tracking schema)
-                    ))   
-
-
-@schema
 class TrialEvent(dj.Imported):
     """
     Trialized events extracted from NIDQ channels with (global) session-based times
@@ -218,7 +208,7 @@ class TrialEvent(dj.Imported):
     -> experiment.BehaviorTrial
     trial_event_id: smallint
     ---
-    -> TrialEventType
+    -> experiment.TrialEventType
     trial_event_time : float  # (s) from session start (global time)
     """    
 
--- pipeline/experiment.py
+++ pipeline/experiment.py
@@ -284,7 +284,9 @@ class TrialEventType(dj.Lookup):
     trial_event_type  : varchar(12)  
     """
     contents = zip(('delay', 'go', 'sample', 'presample', 'trialend',
-                    'videostart', 'videoend', 'bitcodestart', 'choice', 'reward', 'doubledip'))   # Added for foraging
+                    'videostart', 'videoend', 'bitcodestart', 'choice', 'reward', 'doubledip',   # Added for foraging
+                    'bpodstart', 'zaberstep', 'zaberready'    # Events only available from NIDQ channels
+                    ))
 
 
 @schema
