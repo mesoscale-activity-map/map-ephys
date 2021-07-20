09df7857d2a63175dc8c35df4f13893f21179887
han-lab <houhan@gmail.com>
2021-07-20 00:54:30 -0400

    add licks from NI (ephys.ActionEvent)
    # Conflicts:
    #	pipeline/ingest/ephys.py

--- pipeline/ephys.py
+++ pipeline/ephys.py
@@ -214,6 +214,19 @@ class TrialEvent(dj.Imported):
 
 
 @schema
+class ActionEvent(dj.Imported):
+    """
+    Trialized events extracted from NIDQ channels with (global) session-based times
+    """
+    definition = """
+    -> experiment.BehaviorTrial
+    action_event_id: smallint
+    ---
+    -> experiment.ActionEventType
+    action_event_time : float  # (s) from session start (global time)
+    """
+
+@schema
 class UnitNote(dj.Imported):
     definition = """
     -> Unit
