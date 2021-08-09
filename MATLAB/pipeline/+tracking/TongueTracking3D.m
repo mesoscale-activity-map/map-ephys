2b36de3bb4aa0d0a17c103ff898a9ae19cf77704
unknown <liudave.liu@gmail.com>
2021-08-09 14:12:00 -0500

    Add new files from thinh

--- MATLAB/pipeline/+tracking/TongueTracking3D.m
+++ MATLAB/pipeline/+tracking/TongueTracking3D.m
@@ -13,31 +13,36 @@ tongue_z_s    :   longblob
 
 classdef TongueTracking3D < dj.Part
     properties
-        keySource = (v_experiment.Session & 'rig = "RRig-MTL"') & (v_tracking.Tracking & 'tracking_device = "Camera 3"') & (v_tracking.Tracking & 'tracking_device = "Camera 4"');
+        keySource = (experiment.getSchema().v.Session & 'rig = "RRig-MTL"')...
+            & (tracking.getSchema().v.Tracking & 'tracking_device = "Camera 3"')...
+            & (tracking.getSchema().v.Tracking & 'tracking_device = "Camera 4"');
     end
     methods(Access=protected)        
         function makeTuples(self, key)
-            [bot_tongue_x, bot_tongue_y] = fetchn(v_tracking.TrackingTongueTracking & 'tracking_device = "Camera 4"' & key, 'tongue_x', 'tongue_y');
-            [sid_tongue_x, sid_tongue_y] = fetchn(v_tracking.TrackingTongueTracking & 'tracking_device = "Camera 3"' & key, 'tongue_x', 'tongue_y');
+            [bot_tongue_x, bot_tongue_y] = fetchn(tracking.getSchema().v.TrackingTongueTracking & 'tracking_device = "Camera 4"' & key, 'tongue_x', 'tongue_y');
+            [sid_tongue_x, sid_tongue_y] = fetchn(tracking.getSchema().v.TrackingTongueTracking & 'tracking_device = "Camera 3"' & key, 'tongue_x', 'tongue_y');
 
-            load('F:\3dCalib\Calib_Results_stereo.mat')
-
-            Bx=cell2mat(bot_tongue_x(1));
-            By=cell2mat(bot_tongue_y(1));
-            Sx=cell2mat(sid_tongue_x(1));
-            Sy=cell2mat(sid_tongue_y(1));
-
-            x_left_1=[Bx'; By']; x_right_1=[Sx'; Sy'];
-
-            [Xc_1_left,Xc_1_right] = stereo_triangulation(x_left_1,x_right_1,om,T,fc_left,cc_left,kc_left,alpha_c_left,fc_right,cc_right,kc_right,alpha_c_right);
-            insert_key_tongue_position.tongue_x_b = Xc_1_left(1,:);
-            insert_key_tongue_position.tongue_y_b = Xc_1_left(2,:);
-            insert_key_tongue_position.tongue_z_b = Xc_1_left(3,:);
-            insert_key_tongue_position.tongue_x_s = Xc_1_right(1,:);
-            insert_key_tongue_position.tongue_y_s = Xc_1_right(2,:);
-            insert_key_tongue_position.tongue_z_s = Xc_1_right(3,:);
-            insert(self,insert_key_tongue_position);
+            load('Calib_Results_stereo.mat')
             
+            for i = 1:length(bot_jaw_l)
+                Bx=cell2mat(bot_tongue_x(i));
+                By=cell2mat(bot_tongue_y(i));
+                Sx=cell2mat(sid_tongue_x(i));
+                Sy=cell2mat(sid_tongue_y(i));
+
+                x_left_1=[Bx'; By']; x_right_1=[Sx'; Sy'];
+
+                [Xc_1_left,Xc_1_right] = utils.stereo_triangulation(x_left_1,x_right_1,om,T,fc_left,cc_left,kc_left,alpha_c_left,fc_right,cc_right,kc_right,alpha_c_right);
+                trial_key_tongue_position.tongue_x_b = Xc_1_left(1,:);
+                trial_key_tongue_position.tongue_y_b = Xc_1_left(2,:);
+                trial_key_tongue_position.tongue_z_b = Xc_1_left(3,:);
+                trial_key_tongue_position.tongue_x_s = Xc_1_right(1,:);
+                trial_key_tongue_position.tongue_y_s = Xc_1_right(2,:);
+                trial_key_tongue_position.tongue_z_s = Xc_1_right(3,:);
+                key.trial=i;
+                insert(self,insert_key_tongue_position);
+            end
+
         end
     end
-end
+end 
---
+++ MATLAB/pipeline/populate.m
@@ -0,0 +1,7 @@
+%% Executing the populate routines
+
+% run the init
+init
+
+% run the populate routines
+parpopulate(tracking.TongueTracking3D)
