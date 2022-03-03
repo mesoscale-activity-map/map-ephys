399a748ecfe0f12f31e3b5e6a711c1b6001edd37
unknown <liudave.liu@gmail.com>
2022-03-03 11:41:30 -0600

    Add in the BodySVD for GLMFit

--- pipeline/oralfacial_analysis.py
+++ pipeline/oralfacial_analysis.py
@@ -780,6 +780,269 @@ class GLMFitNoLick(dj.Computed):
         #self.insert(units_glm, ignore_extra_fields=True)
 
 @schema
+class GLMFitNoLickBody(dj.Computed):
+    definition = """
+    -> ephys.Unit
+    ---
+    r2_nolick: mediumblob
+    weights_nolick: mediumblob
+    y_nolick: longblob
+    predict_y_nolick: longblob
+    x_nolick: longblob
+    """
+    # mtl sessions only
+    key_source = experiment.Session & v_tracking.TongueTracking3DBot & experiment.Breathing & v_oralfacial_analysis.WhiskerSVD & ephys.Unit & 'rig = "RRig-MTL"'
+    
+    def make(self, key):
+        good_units=ephys.Unit * ephys.ClusterMetric * ephys.UnitStat & key & 'presence_ratio > 0.9' & 'amplitude_cutoff < 0.15' & 'avg_firing_rate > 0.2' & 'isi_violation < 10' & 'unit_amp > 150'
+        unit_keys=good_units.fetch('KEY')
+        bin_width = 0.017
+        num_frame = 1471
+        num_frame_b = 500
+        
+        bad_trial_side,bad_trial_bot,miss_trial_side,miss_trial_bot=(v_oralfacial_analysis.BadVideo & key).fetch('bad_trial_side','bad_trial_bot','miss_trial_side','miss_trial_bot')
+        if (bad_trial_side[0] is None):
+            bad_trial_side[0]=np.array([0])
+        if (miss_trial_side[0] is None):
+            miss_trial_side[0]=np.array([0])
+        if (bad_trial_bot[0] is None):
+            bad_trial_bot[0]=np.array([0])
+        if (miss_trial_bot[0] is None):
+            miss_trial_bot[0]=np.array([0])    
+        bad_trials=np.concatenate((bad_trial_side[0],bad_trial_bot[0],miss_trial_side[0],miss_trial_bot[0]))
+
+        # from the cameras
+        tongue_thr = 0.95
+        traces_s = tracking.Tracking.TongueTracking - [{'trial': tr} for tr in bad_trials] & key & {'tracking_device': 'Camera 3'} 
+        traces_b = tracking.Tracking.TongueTracking - [{'trial': tr} for tr in bad_trials] & key & {'tracking_device': 'Camera 4'}
+        
+        if len(ephys.Unit.TrialSpikes - [{'trial': tr} for tr in bad_trials] & unit_keys[0]) != len(traces_s):
+            print(f'Mismatch in tracking trial and ephys trial number: {key}')
+            return
+        if len(ephys.Unit.TrialSpikes - [{'trial': tr} for tr in bad_trials] & unit_keys[0]) != len(traces_b):
+            print(f'Mismatch in tracking trial and ephys trial number: {key}')
+            return
+        
+        # from the cameras
+        tongue_thr = 0.95
+        trial_key=(v_tracking.TongueTracking3DBot - [{'trial': tr} for tr in bad_trials] & key).fetch('trial', order_by='trial')
+        traces_s = tracking.Tracking.TongueTracking - [{'trial': tr} for tr in bad_trials] & key & {'tracking_device': 'Camera 3'} & [{'trial': tr} for tr in trial_key]
+        traces_b = tracking.Tracking.TongueTracking - [{'trial': tr} for tr in bad_trials] & key & {'tracking_device': 'Camera 4'} & [{'trial': tr} for tr in trial_key]
+        session_traces_s_l = traces_s.fetch('tongue_likelihood', order_by='trial')
+        session_traces_b_l = traces_b.fetch('tongue_likelihood', order_by='trial')
+
+        session_traces_s_l = np.vstack(session_traces_s_l)
+        session_traces_b_l = np.vstack(session_traces_b_l)
+        session_traces_t_l = session_traces_b_l
+        session_traces_t_l[np.where((session_traces_s_l > tongue_thr) & (session_traces_b_l > tongue_thr))] = 1
+        session_traces_t_l[np.where((session_traces_s_l <= tongue_thr) | (session_traces_b_l <= tongue_thr))] = 0
+        session_traces_t_l = np.hstack(session_traces_t_l)
+
+        session_traces_s_l_f = np.vstack(session_traces_s_l)
+        session_traces_b_l_f = np.vstack(session_traces_b_l)
+        session_traces_t_l_f = session_traces_b_l_f
+        session_traces_t_l_f[np.where((session_traces_s_l_f > tongue_thr) & (session_traces_b_l_f > tongue_thr))] = 1
+        session_traces_t_l_f[np.where((session_traces_s_l_f <= tongue_thr) | (session_traces_b_l_f <= tongue_thr))] = 0
+
+        # from 3D calibration
+        traces_s = v_tracking.JawTracking3DSid & key & [{'trial': tr} for tr in trial_key]
+        traces_b = v_tracking.TongueTracking3DBot & key & [{'trial': tr} for tr in trial_key]
+        session_traces_s_y, session_traces_s_x, session_traces_s_z = traces_s.fetch('jaw_y', 'jaw_x', 'jaw_z', order_by='trial')
+        session_traces_b_y, session_traces_b_x, session_traces_b_z = traces_b.fetch('tongue_y', 'tongue_x', 'tongue_z', order_by='trial')
+        session_traces_s_y = stats.zscore(np.vstack(session_traces_s_y),axis=None)
+        session_traces_s_x = stats.zscore(np.vstack(session_traces_s_x),axis=None)
+        session_traces_s_z = stats.zscore(np.vstack(session_traces_s_z),axis=None)
+        session_traces_b_y = np.vstack(session_traces_b_y)
+        traces_y_mean=np.mean(session_traces_b_y[np.where(session_traces_t_l_f == 1)])
+        traces_y_std=np.std(session_traces_b_y[np.where(session_traces_t_l_f == 1)])
+        session_traces_b_y = (session_traces_b_y - traces_y_mean)/traces_y_std
+        session_traces_b_x = np.vstack(session_traces_b_x)
+        traces_x_mean=np.mean(session_traces_b_x[np.where(session_traces_t_l_f == 1)])
+        traces_x_std=np.std(session_traces_b_x[np.where(session_traces_t_l_f == 1)])
+        session_traces_b_x = (session_traces_b_x - traces_x_mean)/traces_x_std
+        session_traces_b_z = np.vstack(session_traces_b_z)
+        traces_z_mean=np.mean(session_traces_b_z[np.where(session_traces_t_l_f == 1)])
+        traces_z_std=np.std(session_traces_b_z[np.where(session_traces_t_l_f == 1)])
+        session_traces_b_z = (session_traces_b_z - traces_z_mean)/traces_z_std
+
+        traces_len = np.size(session_traces_b_z, axis = 1)
+        num_trial = np.size(session_traces_b_z, axis = 0)
+
+        # format the video data
+        session_traces_s_y = np.hstack(session_traces_s_y)
+        session_traces_s_x = np.hstack(session_traces_s_x)
+        session_traces_s_z = np.hstack(session_traces_s_z)
+        session_traces_b_y = np.hstack(session_traces_b_y)
+        session_traces_b_x = np.hstack(session_traces_b_x)
+        session_traces_b_z = np.hstack(session_traces_b_z)
+        # -- moving-average and down-sample
+        window_size = int(bin_width/0.0034)  # sample
+        kernel = np.ones(window_size) / window_size
+        session_traces_s_x = np.convolve(session_traces_s_x, kernel, 'same')
+        session_traces_s_x = session_traces_s_x[window_size::window_size]
+        session_traces_s_y = np.convolve(session_traces_s_y, kernel, 'same')
+        session_traces_s_y = session_traces_s_y[window_size::window_size]
+        session_traces_s_z = np.convolve(session_traces_s_z, kernel, 'same')
+        session_traces_s_z = session_traces_s_z[window_size::window_size]
+        session_traces_b_x = np.convolve(session_traces_b_x, kernel, 'same')
+        session_traces_b_x = session_traces_b_x[window_size::window_size]
+        session_traces_b_y = np.convolve(session_traces_b_y, kernel, 'same')
+        session_traces_b_y = session_traces_b_y[window_size::window_size]
+        session_traces_b_z = np.convolve(session_traces_b_z, kernel, 'same')
+        session_traces_b_z = session_traces_b_z[window_size::window_size]
+        session_traces_t_l = np.convolve(session_traces_t_l, kernel, 'same')
+        session_traces_t_l = session_traces_t_l[window_size::window_size]
+        session_traces_t_l[np.where(session_traces_t_l < 1)] = 0
+        session_traces_s_x = np.reshape(session_traces_s_x,(-1,1))
+        session_traces_s_y = np.reshape(session_traces_s_y,(-1,1))
+        session_traces_s_z = np.reshape(session_traces_s_z,(-1,1))
+        session_traces_b_x = np.reshape(session_traces_b_x * session_traces_t_l, (-1,1))
+        session_traces_b_y = np.reshape(session_traces_b_y * session_traces_t_l, (-1,1))
+        session_traces_b_z = np.reshape(session_traces_b_z * session_traces_t_l, (-1,1))
+
+        # get breathing
+        breathing, breathing_ts = (experiment.Breathing - [{'trial': tr} for tr in bad_trials] & key & [{'trial': tr} for tr in trial_key]).fetch('breathing', 'breathing_timestamps', order_by='trial')
+        good_breathing = breathing
+        for i, d in enumerate(breathing):
+            good_breathing[i] = d[breathing_ts[i] < traces_len*3.4/1000]
+        good_breathing = stats.zscore(np.vstack(good_breathing),axis=None)
+
+        good_breathing = np.hstack(good_breathing)
+        # -- moving-average
+        window_size = int(round(bin_width/(breathing_ts[0][1]-breathing_ts[0][0]),0))  # sample
+        kernel = np.ones(window_size) / window_size
+        good_breathing = np.convolve(good_breathing, kernel, 'same')
+        # -- down-sample
+        good_breathing = good_breathing[window_size::window_size]
+        good_breathing = np.reshape(good_breathing,(-1,1))
+
+        # get whisker
+        session_traces_w = (v_oralfacial_analysis.WhiskerSVD & key).fetch('mot_svd')
+        if len(session_traces_w[0][:,0]) % num_frame != 0:
+            print('Bad videos in bottom view')
+            return
+        else:
+            num_trial_w = int(len(session_traces_w[0][:,0])/num_frame)
+            session_traces_w = np.reshape(session_traces_w[0][:,0], (num_trial_w, num_frame))
+            
+        trial_idx_nat = [d.astype(str) for d in np.arange(num_trial_w)]
+        trial_idx_nat = sorted(range(len(trial_idx_nat)), key=lambda k: trial_idx_nat[k])
+        trial_idx_nat = sorted(range(len(trial_idx_nat)), key=lambda k: trial_idx_nat[k])
+        session_traces_w = session_traces_w[trial_idx_nat,:]
+        session_traces_w_o = stats.zscore(session_traces_w,axis=None)
+        session_traces_w = session_traces_w_o[trial_key-1]
+           
+        session_traces_w = np.hstack(session_traces_w)
+        window_size = int(bin_width/0.0034)  # sample
+        kernel = np.ones(window_size) / window_size
+        session_traces_w = np.convolve(session_traces_w, kernel, 'same')
+        session_traces_w = session_traces_w[window_size::window_size]
+        session_traces_w = np.reshape(session_traces_w,(-1,1))
+        
+        # get body
+        session_traces_b = (v_oralfacial_analysis.BodySVD & key).fetch('mot_svd_body')
+        if len(session_traces_b[0][:,0]) % num_frame_b != 0:
+            print('Bad videos in bottom view')
+            return
+        else:
+            num_trial_b = int(len(session_traces_b[0][:,0])/num_frame_b)
+            session_traces_b = np.reshape(session_traces_b[0][:,0], (num_trial_b, num_frame_b))
+            
+        trial_idx_nat = [d.astype(str) for d in np.arange(num_trial_b)]
+        trial_idx_nat = sorted(range(len(trial_idx_nat)), key=lambda k: trial_idx_nat[k])
+        trial_idx_nat = sorted(range(len(trial_idx_nat)), key=lambda k: trial_idx_nat[k])
+        session_traces_b = session_traces_b[trial_idx_nat,:]
+        session_traces_b_o = stats.zscore(session_traces_b,axis=None)
+        session_traces_b = session_traces_b_o[trial_key-1]
+           
+        session_traces_b = np.hstack(session_traces_b)
+        session_traces_b = np.reshape(session_traces_b,(-1,1))
+        session_traces_b=signal.resample(session_traces_b,len(session_traces_b_x))
+
+        # stimulus
+        lick_onset_time,lick_offset_time=(v_oralfacial_analysis.MovementTiming & key).fetch1('lick_onset','lick_offset')
+
+        all_period_idx=np.arange(len(session_traces_b_y))
+        good_period_idx=[all_period_idx[(all_period_idx*bin_width<lick_onset_time[1]-0.2)]] # restrict by whisking bouts
+        for i,val in enumerate(lick_onset_time[1:]):
+            good_period_idx.append(all_period_idx[(all_period_idx*bin_width<lick_onset_time[i+1]-0.2) & (all_period_idx*bin_width>lick_offset_time[i]+0.2)])
+        good_period_idx.append(all_period_idx[(all_period_idx*bin_width>lick_offset_time[-1]+0.2)])
+        good_period_idx=np.array(good_period_idx)
+        good_period_idx=np.hstack(good_period_idx)
+
+        session_traces_s_x=stats.zscore(session_traces_s_x[good_period_idx])
+        session_traces_s_y=stats.zscore(session_traces_s_y[good_period_idx])
+        session_traces_s_z=stats.zscore(session_traces_s_z[good_period_idx])
+        session_traces_b_x=session_traces_b_x[good_period_idx]
+        traces_x_mean=np.mean(session_traces_b_x[session_traces_b_x != 0])
+        traces_x_std=np.std(session_traces_b_x[session_traces_b_x != 0])
+        session_traces_b_x = (session_traces_b_x - traces_x_mean)/traces_x_std
+        session_traces_b_y=session_traces_b_y[good_period_idx]
+        traces_y_mean=np.mean(session_traces_b_y[session_traces_b_y != 0])
+        traces_y_std=np.std(session_traces_b_y[session_traces_b_y != 0])
+        session_traces_b_y = (session_traces_b_y - traces_y_mean)/traces_y_std
+        session_traces_b_z=session_traces_b_z[good_period_idx]
+        traces_z_mean=np.mean(session_traces_b_z[session_traces_b_z != 0])
+        traces_z_std=np.std(session_traces_b_z[session_traces_b_z != 0])
+        session_traces_b_z = (session_traces_b_z - traces_z_mean)/traces_z_std
+        good_breathing=stats.zscore(good_breathing[good_period_idx])
+        session_traces_w=stats.zscore(session_traces_w[good_period_idx])
+        session_traces_b=stats.zscore(session_traces_b[good_period_idx])
+
+        V_design_matrix = np.concatenate((session_traces_s_x, session_traces_s_y, session_traces_s_z, session_traces_b_x, session_traces_b_y, session_traces_b_z, good_breathing, session_traces_w, session_traces_b), axis=1)
+        
+        #set up GLM
+        sm_log_Link = sm.genmod.families.links.log
+
+        taus = np.arange(-5,6)
+
+        #units_glm = []
+        
+        with InsertBuffer(self, 10, skip_duplicates=True, ignore_extra_fields=True, allow_direct_insert=True) as ib:
+            
+            for unit_key in unit_keys: # loop for each neuron
+    
+                all_spikes=(ephys.Unit.TrialSpikes - [{'trial': tr} for tr in bad_trials] & unit_key & [{'trial': tr} for tr in trial_key]).fetch('spike_times', order_by='trial')
+                
+                good_spikes =all_spikes # get good spikes
+                for i, d in enumerate(good_spikes):
+                    good_spikes[i] = d[d < traces_len*3.4/1000]+traces_len*3.4/1000*i
+                good_spikes = np.hstack(good_spikes)    
+                y, bin_edges = np.histogram(good_spikes, np.arange(0, traces_len*3.4/1000*num_trial, bin_width))
+                y=y[good_period_idx]    
+                
+                r2s=np.zeros(len(taus))
+                weights_t=np.zeros((len(taus),10))
+                predict_ys=np.zeros((len(taus),len(y)))
+                for i, tau in enumerate(taus):
+                    y_roll=np.roll(y,tau)
+                    glm_poiss = sm.GLM(y_roll, sm.add_constant(V_design_matrix), family=sm.families.Poisson(link=sm_log_Link))
+                    
+                    try:
+                        glm_result = glm_poiss.fit()
+                        
+                        sst_val = sum(map(lambda x: np.power(x,2),y_roll-np.mean(y_roll))) 
+                        sse_val = sum(map(lambda x: np.power(x,2),glm_result.resid_response)) 
+                        r2 = 1.0 - sse_val/sst_val
+                        
+                        r2s[i] = r2
+                        
+                        y_roll_t_p=glm_result.predict(sm.add_constant(V_design_matrix))
+                        predict_ys[i,:]=y_roll_t_p
+                        weights_t[i,:] = glm_result.params
+                        
+                    except:
+                        pass
+                    
+                #units_glm.append({**unit_key, 'r2_nolick': r2s, 'weights_nolick': weights_t, 'y_nolick': y, 'predict_y_nolick': predict_ys, 'x_nolick': V_design_matrix})
+                print(unit_key)
+                ib.insert1({**unit_key, 'r2_nolick': r2s, 'weights_nolick': weights_t, 'y_nolick': y, 'predict_y_nolick': predict_ys, 'x_nolick': V_design_matrix})
+                if ib.flush():
+                    pass
+                        
+        #self.insert(units_glm, ignore_extra_fields=True)
+
+@schema
 class GLMFitCAE(dj.Computed):
     definition = """
     -> ephys.Unit
@@ -920,6 +1183,43 @@ class WhiskerSVD(dj.Computed):
         proc = process.run(video_files_l, proc=roi_data)
         
         self.insert1({**key, 'mot_svd': proc['motSVD'][1][:, :3]})
+
+@schema
+class BodySVD(dj.Computed):
+    definition = """
+    -> experiment.Session
+    ---
+    mot_svd_body: longblob
+    """
+    
+    key_source = experiment.Session & 'rig = "RRig-MTL"' & (tracking.Tracking  & 'tracking_device = "Camera 4"')
+    
+    def make(self, key):
+        
+        from facemap import process
+        
+        roi_path = 'H://videos//body//DL027//2021_07_01//DL027_2021_07_01_body_0_proc.npy'
+        roi_data = np.load(roi_path, allow_pickle=True).item()
+        
+        # video_root_dir = pathlib.Path('H:/videos')
+        video_root_dir = pathlib.Path('I:/videos')
+        
+        trials=(tracking_ingest.TrackingIngest.TrackingFile & 'tracking_device = "Camera 4"' & key).fetch('trial')
+        
+        trial_path = (tracking_ingest.TrackingIngest.TrackingFile & 'tracking_device = "Camera 4"' & {'trial': trials[-1]} & key).fetch1('tracking_file')
+        
+        video_path = video_root_dir / trial_path
+        
+        video_path = video_path.parent
+        
+        video_files = list(video_path.glob('*.mp4'))
+        video_files_l = [[str(video_files[0]).replace('bottom','body')]]
+        for ind_trial, file in enumerate(video_files[1:]):
+            video_files_l.append([str(file).replace('bottom','body')])
+            
+        proc = process.run(video_files_l, proc=roi_data)
+        
+        self.insert1({**key, 'mot_svd_body': proc['motSVD'][1][:, :3]})
         
 @schema
 class BottomSVD(dj.Computed):
